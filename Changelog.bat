6d09127dfa0a30b6a306b0b09cf24b62f5f8ce9e
Ajay <78233144+ajay9634@users.noreply.github.com>
2024-12-15 08:50:30 +0530

    Update Changelog.bat

--- Changelog.bat
+++ Changelog.bat
@@ -1,3 +1,4 @@
+@echo off
 echo msgbox "Hello! Ajay Prefix components are updated successfully, Changelog and Files are saved to drive_D/Ajay_prefix/wget_files/ !" , vbinformation+vbSystemModal > %tmp%\tmp.vbs
 cscript /nologo %tmp%\tmp.vbs
 del %tmp%\tmp.vbs
