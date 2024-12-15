65c6517284e132b7c79a058ce04740654201a8b8
Ajay <78233144+ajay9634@users.noreply.github.com>
2024-12-15 07:43:41 +0530

    Create Changelog.bat

---
+++ Changelog.bat
@@ -0,0 +1,5 @@
+echo msgbox "Hello! Components scripts are updated ,
+Changelog - added some d3d
+          - added some physx" , vbinformation+vbSystemModal > %tmp%\tmp.vbs
+cscript /nologo %tmp%\tmp.vbs
+del %tmp%\tmp.vbs￼Enter
