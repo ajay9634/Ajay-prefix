e54077706a12e2525bdded876dea5167dfc00541
Ajay <78233144+ajay9634@users.noreply.github.com>
2024-12-18 09:44:13 +0530

    Add files via upload

---
+++ Start Menu/Ajay Start Menu/1.components/Files/DOSBox-SVN-Daum 32bit.bat
@@ -0,0 +1,24 @@
+@echo off
+color 0a
+echo *** Enjoy Dosbox games ***
+echo *** script made by Ajay ***
+
+:: Simulating bold with color and emphasis
+color 0A
+echo.
+echo *** Downloading DOSBox-SVN-Daum ***
+IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\DOSBox-SVN-Daum.7z" (
+    wget -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/winlator-ajay/main/My-files/DOSBox-SVN-Daum.7z
+    copy /s /y D:\Ajay_prefix\wget_files\temp\DOSBox-SVN-Daum.7z D:\Ajay_prefix\wget_files\Files\DOSBox-SVN-Daum.7z /E /H /C /I
+) ELSE (
+    ECHO DOSBox-SVN-Daum already exists.
+)
+
+
+color 1f
+echo *** Extracting ...***
+D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\Files\DOSBox-SVN-Daum.7z -oD:\Ajay_prefix\wget_files\my_apps\ -r -y >NUL 2>&1
+
+echo *** opening dosbox-32bit ***
+Start D:\Ajay_prefix\wget_files\my_apps\DOSBox-SVN-Daum\dosbox.exe
+echo ************************************************
