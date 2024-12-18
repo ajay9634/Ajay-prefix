d45d14af79d575550f7158b4d0a89f56d7a2814d
Ajay <78233144+ajay9634@users.noreply.github.com>
2024-12-18 08:52:48 +0530

    Add files via upload

---
+++ Start Menu/Ajay Start Menu/1.components/dsound/install dsound of wine-8.18.bat
@@ -0,0 +1,30 @@
+@echo off
+color 0a
+echo *** wait....***
+echo *** deleting temp files...***
+rmdir /S /Q "D:/Ajay_prefix/wget_files/temp"
+mkdir "D:/Ajay_prefix/wget_files/temp"
+echo *** deleted temp files ***
+echo .
+echo *** script made by Ajay ***
+
+:: Simulating bold with color and emphasis
+color 0A
+echo.
+echo *** Downloading dsound ***
+IF NOT EXIST "D:\Ajay_prefix\wget_files\dsound\dsound.7z" (
+    wget -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/winlator-ajay/main/My-files/dsound.7z
+    copy /s /y D:\Ajay_prefix\wget_files\temp\dsound.7z D:\Ajay_prefix\wget_files\dsound\dsound.7z /E /H /C /I
+) ELSE (
+    ECHO dsound file already exists.
+)
+
+:: Simulate download progress only for subsequent parts
+
+color 1f
+echo *** Extracting ...***
+D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\dsound\dsound.7z -oD:\Ajay_prefix\wget_files\temp\ -r -y >NUL 2>&1
+echo *** installing...***
+Xcopy /s /y D:\Ajay_prefix\wget_files\temp\dsound-wine-8.18\ C:\windows\ /E /H /C /I
+)
+echo.************************************************
