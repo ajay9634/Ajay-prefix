81b266202a75e392bfdafb18b3c9bc6f9040fcb4
Ajay <78233144+ajay9634@users.noreply.github.com>
2024-12-19 16:18:29 +0530

    Add files via upload

---
+++ Start Menu/Ajay Start Menu/1.components/Dxvk/dxvk-fix(install it after any dxvk).bat
@@ -0,0 +1,12 @@
+@echo off
+echo for dxvk 2.0 and later
+echo Installing dxvk-fix ..
+echo This may fix some games like far cry3 
+copy /s /y C:\windows\system32\d3d10core.dll C:\windows\system32\d3d10.dll /E /H /C /I
+copy /s /y C:\windows\system32\d3d10core.dll C:\windows\system32\d3d10_1.dll /E /H /C /I
+copy /s /y C:\windows\syswow64\d3d10core.dll C:\windows\syswow64\d3d10.dll /E /H /C /I
+copy /s /y C:\windows\syswow64\d3d10core.dll C:\windows\syswow64\d3d10_1.dll /E /H /C /I
+
+
+echo ************************************************
+pause
