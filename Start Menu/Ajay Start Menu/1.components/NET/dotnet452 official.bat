42593818e8796c0c6bd332d32e96ca93c53aa86b
Ajay <78233144+ajay9634@users.noreply.github.com>
2024-12-20 17:02:22 +0530

    Update dotnet452 official.bat

--- Start Menu/Ajay Start Menu/1.components/NET/dotnet452 official.bat
+++ Start Menu/Ajay Start Menu/1.components/NET/dotnet452 official.bat
@@ -7,7 +7,7 @@ echo ==================================
 echo.
 echo *** Now choose an option - ***
 echo.
-ECHO 1. Install .NET Framework 4.0 official
+ECHO 1. Install .NET Framework 4.5.2 official
 ECHO 2. Cancel or exit
 set choice=
 set /p choice=Type the number to select an option.
