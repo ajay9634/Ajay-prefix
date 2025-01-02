69f1d52c00c6d7de99c3d290b9f36d12f283b209
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-02 10:08:35 +0530

    Delete msgbox.bat

--- msgbox.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo msgbox "Hello! Ajay Prefix components are updated successfully, Changelog and Files are saved to drive_D/Ajay_prefix/wget_files/ !" , vbinformation+vbSystemModal > %tmp%\tmp.vbs
-cscript /nologo %tmp%\tmp.vbs
-del %tmp%\tmp.vbs
