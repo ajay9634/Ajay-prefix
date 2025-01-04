d64a166e1a9510eb244e0940ee36f4d8b31fc07a
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-04 22:04:40 +0530

    Update msgbox.bat

--- msgbox.bat
+++ msgbox.bat
@@ -1,19 +1,19 @@
 @echo off
-mkdir D:\Ajay_prefix\wget_files\d3d
-mkdir D:\Ajay_prefix\wget_files\dsound
-mkdir D:\Ajay_prefix\wget_files\Files
-mkdir D:\Ajay_prefix\wget_files\Fixes
-mkdir D:\Ajay_prefix\wget_files\Game-fixes
-mkdir D:\Ajay_prefix\wget_files\NET
-mkdir D:\Ajay_prefix\wget_files\physx
-mkdir D:\Ajay_prefix\wget_files\temp
-mkdir D:\Ajay_prefix\wget_files\my_apps
-mkdir D:\Ajay_prefix\save_data\users\Registry_backup
-mkdir D:\Ajay_prefix\save_data\users\xuser\AppData\Local
-mkdir D:\Ajay_prefix\save_data\users\xuser\AppData\Roaming
-mkdir D:\Ajay_prefix\save_data\users\xuser\Documents
-mkdir "D:\Ajay_prefix\save_data\users\xuser\Documents\My Games"
-mkdir "D:\Ajay_prefix\save_data\users\xuser\"Saved Games
+mkdir D:\Ajay_prefix\wget_files\d3d 2>nul
+mkdir D:\Ajay_prefix\wget_files\dsound 2>nul
+mkdir D:\Ajay_prefix\wget_files\Files 2>nul
+mkdir D:\Ajay_prefix\wget_files\Fixes 2>nul
+mkdir D:\Ajay_prefix\wget_files\Game-fixes 2>nul
+mkdir D:\Ajay_prefix\wget_files\NET 2>nul
+mkdir D:\Ajay_prefix\wget_files\physx 2>nul
+mkdir D:\Ajay_prefix\wget_files\temp 2>nul
+mkdir D:\Ajay_prefix\wget_files\my_apps 2>nul
+mkdir D:\Ajay_prefix\save_data\users\Registry_backup 2>nul
+mkdir D:\Ajay_prefix\save_data\users\xuser\AppData\Local 2>nul
+mkdir D:\Ajay_prefix\save_data\users\xuser\AppData\Roaming 2>nul
+mkdir D:\Ajay_prefix\save_data\users\xuser\Documents 2>nul
+mkdir "D:\Ajay_prefix\save_data\users\xuser\Documents\My Games" 2>nul
+mkdir "D:\Ajay_prefix\save_data\users\xuser\"Saved Games 2>nul
 
 echo msgbox "Hello! Ajay Prefix components are updated successfully, Changelog and Files are saved to drive_D/Ajay_prefix/wget_files/ !" , vbinformation+vbSystemModal > %tmp%\tmp.vbs
 cscript /nologo %tmp%\tmp.vbs
