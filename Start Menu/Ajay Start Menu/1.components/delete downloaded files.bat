9ee3ac95dbe328c9ad54e2242e035f776a6307f9
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-11 08:39:06 +0530

    Update delete downloaded files.bat

--- Start Menu/Ajay Start Menu/1.components/delete downloaded files.bat
+++ Start Menu/Ajay Start Menu/1.components/delete downloaded files.bat
@@ -1,6 +1,9 @@
 
 @echo off
+echo deleting temp files
 rmdir /S /Q "D:/Ajay_prefix/wget_files/temp" > NUL 2>&1
 mkdir "D:/Ajay_prefix/wget_files/temp"
 rmdir /S /Q "C:\ProgramData\Microsoft\Windows\Start Menu\Ajay Start Menu\1.components" > NUL 2>&1
+echo Now delete downloaded files manually
 start "" "C:\windows\7zFM.exe" "D:\ajay_prefix\wget_files\"
+
