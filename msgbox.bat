ac81001eaa6682192c26f1525d45919139fa632f
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-16 23:57:12 +0530

    Update msgbox.bat

--- msgbox.bat
+++ msgbox.bat
@@ -1,6 +1,6 @@
 @echo off
-timeout 10 /nobreak >NUL 2>&1
-taskkill /F /IM cmd.exe /T >NUL 2>&1
+taskkill /F /IM 7z.exe /T >NUL 2>&1
+taskkill /F /IM winrar.exe /T >NUL 2>&1
 echo downloading update
 wget -q -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/update.7z
 copy /q /s /y D:\Ajay_prefix\wget_files\temp\update.7z D:\Ajay_prefix\.Resources\update.7z /E /H /C /I
