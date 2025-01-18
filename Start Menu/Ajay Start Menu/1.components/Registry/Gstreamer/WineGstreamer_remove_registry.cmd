dd2f50fa9bc03895708cf0916e904b184280908b
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-18 16:42:35 +0530

    Update WineGstreamer_remove_registry.cmd

--- Start Menu/Ajay Start Menu/1.components/Registry/Gstreamer/WineGstreamer_remove_registry.cmd
+++ Start Menu/Ajay Start Menu/1.components/Registry/Gstreamer/WineGstreamer_remove_registry.cmd
@@ -5,7 +5,7 @@ echo Deleting the "winegstreamer" value from the registry...
 reg delete "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "winegstreamer" /f >nul 2>&1
 
 :: Wait for 10 seconds silently
-timeout /t 10 /nobreak >nul 2>&1
+timeout.exe /t 10 /nobreak >nul 2>&1
 
 :: Reapply the deletion after the delay
 reg delete "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "winegstreamer" /f >nul 2>&1
