8243421d68a218b0299e899a738487dbe0f97697
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-18 16:47:11 +0530

    Update Renderer_OpenGL.cmd

--- Start Menu/Ajay Start Menu/1.components/Registry/Renderer/Renderer_OpenGL.cmd
+++ Start Menu/Ajay Start Menu/1.components/Registry/Renderer/Renderer_OpenGL.cmd
@@ -5,7 +5,7 @@ echo Setting the "renderer" to "gl" in the registry...
 reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "renderer" /t REG_SZ /d "gl" /f >nul 2>&1
 
 :: Wait for 10 seconds silently
-timeout /t 10 /nobreak >nul 2>&1
+timeout.exe /t 10 /nobreak >nul 2>&1
 
 :: Reapply the registry setting after the delay
 reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "renderer" /t REG_SZ /d "gl" /f >nul 2>&1
