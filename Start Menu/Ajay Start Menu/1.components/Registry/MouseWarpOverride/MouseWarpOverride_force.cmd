cec5d361398c6d8f0b864c637ab4f839cdc44f31
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-18 16:44:36 +0530

    Update MouseWarpOverride_force.cmd

--- Start Menu/Ajay Start Menu/1.components/Registry/MouseWarpOverride/MouseWarpOverride_force.cmd
+++ Start Menu/Ajay Start Menu/1.components/Registry/MouseWarpOverride/MouseWarpOverride_force.cmd
@@ -5,7 +5,7 @@ echo Setting the "MouseWarpOverride" to "force" in the registry...
 reg add "HKEY_CURRENT_USER\Software\Wine\DirectInput" /v "MouseWarpOverride" /t REG_SZ /d "force" /f >nul 2>&1
 
 :: Wait for 10 seconds silently
-timeout /t 10 /nobreak >nul 2>&1
+timeout.exe /t 10 /nobreak >nul 2>&1
 
 :: Reapply the registry setting after the delay
 reg add "HKEY_CURRENT_USER\Software\Wine\DirectInput" /v "MouseWarpOverride" /t REG_SZ /d "force" /f >nul 2>&1
