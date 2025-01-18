e19efd54f4d6292d32d9a3b917f4dcd9826acc96
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-18 16:51:26 +0530

    Update disable 3.0(software mode).cmd

--- Start Menu/Ajay Start Menu/1.components/Registry/Shader model/disable 3.0(software mode).cmd
+++ Start Menu/Ajay Start Menu/1.components/Registry/Shader model/disable 3.0(software mode).cmd
@@ -5,7 +5,7 @@ echo Setting "VertexShaderMode" to "software" in the registry...
 reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "VertexShaderMode" /t REG_SZ /d "software" /f >nul 2>&1
 
 :: Wait for 10 seconds silently
-timeout /t 10 /nobreak >nul 2>&1
+timeout.exe /t 10 /nobreak >nul 2>&1
 
 :: Reapply the registry setting after the delay
 reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "VertexShaderMode" /t REG_SZ /d "software" /f >nul 2>&1
