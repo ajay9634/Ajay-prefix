af2c7c321be7afab6bfef85eda710bd2765bd45d
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-18 17:36:39 +0530

    Create Fallout 4 (enable mods).cmd

---
+++ Start Menu/Ajay Start Menu/1.components/Games-fixes/Registry-fixes/Fallout 4 (enable mods).cmd
@@ -0,0 +1,13 @@
+@echo off
+echo Enabling Mods for Fallout 4 in the registry...
+
+:: Set Mods Enabled to true (1)
+reg add "HKCU\Software\Bethesda Softworks\Fallout4" /v "bEnableMods" /t REG_DWORD /d 1 /f >nul 2>&1
+
+:: Wait 10 seconds to ensure changes are applied
+timeout.exe /t 10 /nobreak >nul 2>&1
+
+:: Reapply the registry action silently
+reg add "HKCU\Software\Bethesda Softworks\Fallout4" /v "bEnableMods" /t REG_DWORD /d 1 /f >nul 2>&1
+
+echo Mods have been successfully enabled.
