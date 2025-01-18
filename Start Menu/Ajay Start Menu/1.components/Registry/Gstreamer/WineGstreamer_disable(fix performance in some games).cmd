0082ac3e8af5ce25196ed042cc8ac5c480431c20
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-18 16:42:15 +0530

    Update WineGstreamer_disable(fix performance in some games).cmd

--- Start Menu/Ajay Start Menu/1.components/Registry/Gstreamer/WineGstreamer_disable(fix performance in some games).cmd
+++ Start Menu/Ajay Start Menu/1.components/Registry/Gstreamer/WineGstreamer_disable(fix performance in some games).cmd
@@ -5,7 +5,7 @@ echo Clearing the "winegstreamer" value in the registry...
 reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "winegstreamer" /t REG_SZ /d "" /f >nul 2>&1
 
 :: Wait for 10 seconds silently
-timeout /t 10 /nobreak >nul 2>&1
+timeout.exe /t 10 /nobreak >nul 2>&1
 
 :: Reapply the registry setting after the delay
 reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "winegstreamer" /t REG_SZ /d "" /f >nul 2>&1
