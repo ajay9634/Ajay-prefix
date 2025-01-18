a2514607474ce1960f81826c2c3c69f910a6038b
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-18 16:47:27 +0530

    Update Renderer_Vulkan(not recommended).cmd

--- Start Menu/Ajay Start Menu/1.components/Registry/Renderer/Renderer_Vulkan(not recommended).cmd
+++ Start Menu/Ajay Start Menu/1.components/Registry/Renderer/Renderer_Vulkan(not recommended).cmd
@@ -5,7 +5,7 @@ echo Setting the "renderer" to "vulkan" in the registry...
 reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "renderer" /t REG_SZ /d "vulkan" /f >nul 2>&1
 
 :: Wait for 10 seconds silently
-timeout /t 10 /nobreak >nul 2>&1
+timeout.exe /t 10 /nobreak >nul 2>&1
 
 :: Reapply the registry setting after the delay
 reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "renderer" /t REG_SZ /d "vulkan" /f >nul 2>&1
