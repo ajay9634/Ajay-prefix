66ab6e4f16c8aabf07829a9a6d0d52be9edac659
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-18 12:24:26 +0530

    Add files via upload

---
+++ Start Menu/Ajay Start Menu/1.components/Registry/Wine multi-threading/enable (+performance).cmd
@@ -0,0 +1,9 @@
+@echo off
+echo Enabling UseXRandR in Wine...
+
+reg add "HKEY_CURRENT_USER\Software\Wine\X11 Driver" /v "UseXRandR" /t REG_SZ /d "enabled" /f >nul 2>&1
+
+timeout.exe /t 10 /nobreak >nul 2>&1
+
+echo UseXRandR has been enabled successfully in Wine.
+exit
