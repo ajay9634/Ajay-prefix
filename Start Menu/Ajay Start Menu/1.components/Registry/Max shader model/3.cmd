8b0744b38ea2cc989f0f91ec88922b321d04d5cb
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-18 12:01:55 +0530

    Add files via upload

---
+++ Start Menu/Ajay Start Menu/1.components/Registry/Max shader model/3.cmd
@@ -0,0 +1,7 @@
+@echo off
+echo Configuring Wine to use Shader Model 3...
+reg add "HKCU\Software\Wine\Direct3D" /v MaxShaderModel /t REG_DWORD /d 3 /f >nul 2>&1
+timeout.exe 10 /nobreak >nul 2>&1
+reg add "HKCU\Software\Wine\Direct3D" /v MaxShaderModel /t REG_DWORD /d 3 /f >nul 2>&1
+echo Shader Model 3 has been configured.
+exit /b
