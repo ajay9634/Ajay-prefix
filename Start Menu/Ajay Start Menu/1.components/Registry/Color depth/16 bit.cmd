5dcf2857c47ea6cfb1f9e8542ac6445d34a796d0
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-18 16:39:54 +0530

    Update 16 bit.cmd

--- Start Menu/Ajay Start Menu/1.components/Registry/Color depth/16 bit.cmd
+++ Start Menu/Ajay Start Menu/1.components/Registry/Color depth/16 bit.cmd
@@ -5,7 +5,7 @@ echo Setting the "ColorDepth" to "16" in the registry...
 reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "ColorDepth" /t REG_SZ /d "16" /f >nul 2>&1
 
 :: Wait for 10 seconds silently
-timeout /t 10 /nobreak >nul 2>&1
+timeout.exe /t 10 /nobreak >nul 2>&1
 
 :: Reapply the registry setting after the delay
 reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "ColorDepth" /t REG_SZ /d "16" /f >nul 2>&1
