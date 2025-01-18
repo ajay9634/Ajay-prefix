1b542fe48988ea38291b3bc1b8a1dbc63f49bc51
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-18 16:34:39 +0530

    Create quartz(disable).cmd

---
+++ Start Menu/Ajay Start Menu/1.components/Registry/Quartz/quartz(disable).cmd
@@ -0,0 +1,13 @@
+@echo off
+echo Disabling Quartz in the registry...
+
+:: Set Quartz to disabled
+reg add "HKCU\Software\Wine\DllOverrides" /v "quartz" /t REG_SZ /d "disabled" /f >nul 2>&1
+
+:: Wait 10 seconds to ensure changes are applied
+timeout /t 10 /nobreak >nul 2>&1
+
+:: Reapply the registry actions silently
+reg add "HKCU\Software\Wine\DllOverrides" /v "quartz" /t REG_SZ /d "disabled" /f >nul 2>&1
+
+echo Quartz has been successfully disabled.
