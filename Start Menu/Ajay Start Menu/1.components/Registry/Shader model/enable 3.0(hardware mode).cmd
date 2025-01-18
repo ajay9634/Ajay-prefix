84a218f450b4680b212be910f3c01a97c9840fa7
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-18 16:52:12 +0530

    Update enable 3.0(hardware mode).cmd

--- Start Menu/Ajay Start Menu/1.components/Registry/Shader model/enable 3.0(hardware mode).cmd
+++ Start Menu/Ajay Start Menu/1.components/Registry/Shader model/enable 3.0(hardware mode).cmd
@@ -5,7 +5,7 @@ echo Setting "VertexShaderMode" to "hardware" in the registry...
 reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "VertexShaderMode" /t REG_SZ /d "hardware" /f >nul 2>&1
 
 :: Wait for 10 seconds silently
-timeout /t 10 /nobreak >nul 2>&1
+timeout.exe /t 10 /nobreak >nul 2>&1
 
 :: Reapply the registry setting after the delay
 reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "VertexShaderMode" /t REG_SZ /d "hardware" /f >nul 2>&1
