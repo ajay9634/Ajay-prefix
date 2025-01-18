b75a6f74ded9a061c3d5cb0413dd6b8eec501f79
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-18 16:41:55 +0530

    Update WineGstreamer(native).cmd

--- Start Menu/Ajay Start Menu/1.components/Registry/Gstreamer/WineGstreamer(native).cmd
+++ Start Menu/Ajay Start Menu/1.components/Registry/Gstreamer/WineGstreamer(native).cmd
@@ -5,7 +5,7 @@ echo Configuring winegstreamer to use native version in the registry...
 reg add "HKCU\Software\Wine\DllOverrides" /v "winegstreamer" /t REG_SZ /d "native" /f >nul 2>&1
 
 :: Wait 10 seconds to ensure changes are applied
-timeout /t 10 /nobreak >nul 2>&1
+timeout.exe /t 10 /nobreak >nul 2>&1
 
 :: Reapply the registry actions silently
 reg add "HKCU\Software\Wine\DllOverrides" /v "winegstreamer" /t REG_SZ /d "native" /f >nul 2>&1
