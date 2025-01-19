99c0bc101cdc85f8fd6cc9ebc3964dc3ee97fd70
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-19 16:41:08 +0530

    Update and rename Test.bat to Manage downloaded Wine.bat

---
+++ Start Menu/Ajay Start Menu/1.components/Wine Mono/Manage downloaded Wine.bat
@@ -0,0 +1,7 @@
+@echo off
+echo deleting temp files
+rmdir /S /Q "D:/Ajay_prefix/wget_files/temp" > NUL 2>&1
+mkdir "D:/Ajay_prefix/wget_files/temp"
+rmdir /S /Q "C:\ProgramData\Microsoft\Windows\Start Menu\Ajay Start Menu\1.components" > NUL 2>&1
+echo Now delete downloaded mono manually
+start "" "C:\windows\7zFM.exe" "D:\ajay_prefix\wget_files\mono\"
