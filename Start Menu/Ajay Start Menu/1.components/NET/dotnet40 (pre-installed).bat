d4ae1b5e1902c0f86d16828ce6d9e66117733021
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-20 09:24:15 +0530

    Update dotnet40 (pre-installed).bat

--- Start Menu/Ajay Start Menu/1.components/NET/dotnet40 (pre-installed).bat
+++ Start Menu/Ajay Start Menu/1.components/NET/dotnet40 (pre-installed).bat
@@ -34,7 +34,7 @@ color 0A
 echo.
 echo *** Downloading part01.rar ***
 IF NOT EXIST "D:\Ajay_prefix\wget_files\NET\Net_framework_4.0.part01.rar" (
-    wget -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Net_framework_4.0.part01.rar
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Net_framework_4.0.part01.rar
     copy /s /y D:\Ajay_prefix\wget_files\temp\Net_framework_4.0.part01.rar D:\Ajay_prefix\wget_files\NET\Net_framework_4.0.part01.rar /E /H /C /I
 ) ELSE (
     ECHO Part 01 already exists.
