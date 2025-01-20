29e993f506ac32ac5590c6bf0dff382e50e7111f
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-20 09:43:07 +0530

    Update PhysX-8.04.25(Medal of Honor airborne).bat

--- Start Menu/Ajay Start Menu/1.components/Physx/PhysX-8.04.25(Medal of Honor airborne).bat
+++ Start Menu/Ajay Start Menu/1.components/Physx/PhysX-8.04.25(Medal of Honor airborne).bat
@@ -13,7 +13,7 @@ color 0A
 echo.
 echo *** Downloading part01.rar ***
 IF NOT EXIST "D:\Ajay_prefix\wget_files\physx\PhysX_8.04.25_SystemSoftware.part1.rar" (
-    wget -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/PhysX_8.04.25_SystemSoftware.part1.rar
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/PhysX_8.04.25_SystemSoftware.part1.rar
     copy /s /y D:\Ajay_prefix\wget_files\temp\PhysX_8.04.25_SystemSoftware.part1.rar D:\Ajay_prefix\wget_files\physx\PhysX_8.04.25_SystemSoftware.part1.rar /E /H /C /I
 ) ELSE (
     ECHO Part 01 already exists.
