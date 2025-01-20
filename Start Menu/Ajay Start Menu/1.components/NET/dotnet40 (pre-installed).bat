bc86a185cfc14d849ac01ea9826f4725a1fd3070
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-20 09:42:36 +0530

    Update dotnet40 (pre-installed).bat

--- Start Menu/Ajay Start Menu/1.components/NET/dotnet40 (pre-installed).bat
+++ Start Menu/Ajay Start Menu/1.components/NET/dotnet40 (pre-installed).bat
@@ -45,7 +45,7 @@ color 0A
 echo.
 echo *** Downloading part02.rar ***
 IF NOT EXIST "D:\Ajay_prefix\wget_files\NET\Net_framework_4.0.part02.rar" (
-    wget -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Net_framework_4.0.part02.rar
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Net_framework_4.0.part02.rar
     copy /s /y D:\Ajay_prefix\wget_files\temp\Net_framework_4.0.part02.rar D:\Ajay_prefix\wget_files\NET\Net_framework_4.0.part02.rar /E /H /C /I
 ) ELSE (
     ECHO Part 02 already exists.
@@ -56,7 +56,7 @@ color 0A
 echo.
 echo *** Downloading part03.rar ***
 IF NOT EXIST "D:\Ajay_prefix\wget_files\NET\Net_framework_4.0.part03.rar" (
-    wget -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Net_framework_4.0.part03.rar
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Net_framework_4.0.part03.rar
     copy /s /y D:\Ajay_prefix\wget_files\temp\Net_framework_4.0.part03.rar D:\Ajay_prefix\wget_files\NET\Net_framework_4.0.part03.rar /E /H /C /I
 ) ELSE (
     ECHO Part 03 already exists.
@@ -66,7 +66,7 @@ color 0A
 echo.
 echo *** Downloading part04.rar ***
 IF NOT EXIST "D:\Ajay_prefix\wget_files\NET\Net_framework_4.0.part04.rar" (
-    wget -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Net_framework_4.0.part04.rar
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Net_framework_4.0.part04.rar
     copy /s /y D:\Ajay_prefix\wget_files\temp\Net_framework_4.0.part04.rar D:\Ajay_prefix\wget_files\NET\Net_framework_4.0.part04.rar /E /H /C /I
 ) ELSE (
     ECHO Part 04 already exists.
@@ -76,7 +76,7 @@ color 0A
 echo.
 echo *** Downloading part05.rar ***
 IF NOT EXIST "D:\Ajay_prefix\wget_files\NET\Net_framework_4.0.part05.rar" (
-    wget -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Net_framework_4.0.part05.rar
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Net_framework_4.0.part05.rar
     copy /s /y D:\Ajay_prefix\wget_files\temp\Net_framework_4.0.part05.rar D:\Ajay_prefix\wget_files\NET\Net_framework_4.0.part05.rar /E /H /C /I
 ) ELSE (
     ECHO Part 05 already exists.
@@ -86,7 +86,7 @@ color 0A
 echo.
 echo *** Downloading part06.rar ***
 IF NOT EXIST "D:\Ajay_prefix\wget_files\NET\Net_framework_4.0.part06.rar" (
-    wget -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Net_framework_4.0.part06.rar
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Net_framework_4.0.part06.rar
     copy /s /y D:\Ajay_prefix\wget_files\temp\Net_framework_4.0.part06.rar D:\Ajay_prefix\wget_files\NET\Net_framework_4.0.part06.rar /E /H /C /I
 ) ELSE (
     ECHO Part 06 already exists.
@@ -96,7 +96,7 @@ color 0A
 echo.
 echo *** Downloading part07.rar ***
 IF NOT EXIST "D:\Ajay_prefix\wget_files\NET\Net_framework_4.0.part07.rar" (
-    wget -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Net_framework_4.0.part07.rar
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Net_framework_4.0.part07.rar
     copy /s /y D:\Ajay_prefix\wget_files\temp\Net_framework_4.0.part07.rar D:\Ajay_prefix\wget_files\NET\Net_framework_4.0.part07.rar /E /H /C /I
 ) ELSE (
     ECHO Part 07 already exists.
