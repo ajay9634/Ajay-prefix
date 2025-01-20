60652bf7ff854b30fdf4e386ec048c98d02acb67
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-20 09:26:30 +0530

    Update xnafx30_redist.bat

--- Start Menu/Ajay Start Menu/1.components/NET/xnafx30_redist.bat
+++ Start Menu/Ajay Start Menu/1.components/NET/xnafx30_redist.bat
@@ -15,7 +15,7 @@ color 0A
 echo.
 echo *** Downloading xnafx30_redist  ***
 IF NOT EXIST "D:\Ajay_prefix\wget_files\NET\xnafx30_redist.7z" (
-    wget -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/xnafx30_redist.7z
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/xnafx30_redist.7z
     copy /s /y D:\Ajay_prefix\wget_files\temp\xnafx30_redist.7z D:\Ajay_prefix\wget_files\NET\xnafx30_redist.7z /E /H /C /I
 ) ELSE (
     ECHO xnafx30_redist file already exists.
