cf902de2e70d0cf7706aa9a383bdde1f02be2e2a
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-20 09:40:11 +0530

    Update vkd3d-proton-dev.281024.bat

--- Start Menu/Ajay Start Menu/1.components/Vkd3d/vkd3d-proton-dev.281024.bat
+++ Start Menu/Ajay Start Menu/1.components/Vkd3d/vkd3d-proton-dev.281024.bat
@@ -13,7 +13,7 @@ color 0A
 echo.
 echo *** Downloading vkd3d-proton-dev.281024  ***
 IF NOT EXIST "D:\Ajay_prefix\wget_files\d3d\vkd3d-proton-dev.281024.7z" (
-    wget -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/vkd3d-proton-dev.281024.7z
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/vkd3d-proton-dev.281024.7z
     copy /s /y D:\Ajay_prefix\wget_files\temp\vkd3d-proton-dev.281024.7z D:\Ajay_prefix\wget_files\d3d\vkd3d-proton-dev.281024.7z /E /H /C /I
 ) ELSE (
     ECHO vkd3d-proton-dev.281024.7z file already exists.
