3d04929a9e3d29fdf87726895396c327cf63d89c
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-22 13:34:25 +0530

    Add files via upload

---
+++ Start Menu/Ajay Start Menu/1.components/Codecs/DiracDirectShowFilter-1.0.2.bat
@@ -0,0 +1,24 @@
+@echo off
+color 0a
+echo *** Installing DiracDirectShowFilter-1.0.2 ***
+echo *** script made by Ajay ***
+
+:: Simulating bold with color and emphasis
+color 0A
+echo.
+echo *** Downloading codecs ***
+IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\codecs_collection.7z" (
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/codecs_collection.7z
+    copy /s /y D:\Ajay_prefix\wget_files\temp\codecs_collection.7z D:\Ajay_prefix\wget_files\Files\codecs_collection.7z /E /H /C /I
+) ELSE (
+    ECHO backup file is already exist.
+)
+
+
+color 1f
+echo *** Extracting ...***
+D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\Files\codecs_collection.7z -oD:\Ajay_prefix\wget_files\temp\ -p1 -r -y >NUL 2>&1
+
+echo *** install DiracDirectShowFilter-1.0.2 manually ***
+Start /wait D:\Ajay_prefix\wget_files\temp\DiracDirectShowFilter-1.0.2.exe
+echo ************************************************
