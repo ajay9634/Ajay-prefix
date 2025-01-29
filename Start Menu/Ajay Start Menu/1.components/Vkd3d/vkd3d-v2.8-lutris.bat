5c088bab1f186793c146e36725baa85c9b5eb6fa
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-29 08:54:10 +0530

    Update vkd3d-v2.8-lutris.bat

--- Start Menu/Ajay Start Menu/1.components/Vkd3d/vkd3d-v2.8-lutris.bat
+++ Start Menu/Ajay Start Menu/1.components/Vkd3d/vkd3d-v2.8-lutris.bat
@@ -2,8 +2,8 @@
 color 0a
 echo *** wait....***
 echo *** deleting temp files...***
-rmdir /S /Q "D:/Ajay_prefix/wget_files/temp"
-mkdir "D:/Ajay_prefix/wget_files/temp"
+rmdir /S /Q "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
+mkdir "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
 echo *** deleted temp files ***
 echo .
 echo *** script made by Ajay ***
