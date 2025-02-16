ad4ef3d196ac695c7b5344f3514e2ffb6474d773
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-02-16 11:02:57 +0530

    Update and rename install missing dlls as skip.bat to install missing dlls v1.0 as skip.bat

--- Start Menu/Ajay Start Menu/1.components/Fixes/Vcredist/install missing dlls v1.0 as skip.bat
+++ Start Menu/Ajay Start Menu/1.components/Fixes/Vcredist/install missing dlls v1.0 as skip.bat
@@ -12,9 +12,9 @@ echo *** script made by Ajay ***
 color 0A
 echo.
 echo *** Downloading Missing_dlls ***
-IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\Missing_dlls.7z" (
-    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Missing_dlls.7z
-    copy /s /y D:\Ajay_prefix\wget_files\temp\Missing_dlls.7z D:\Ajay_prefix\wget_files\Files\Missing_dlls.7z /E /H /C /I
+IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\Missing_dlls_v1.0.7z" (
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Missing_dlls_v1.0.7z
+    copy /s /y D:\Ajay_prefix\wget_files\temp\Missing_dlls_v1.0.7z D:\Ajay_prefix\wget_files\Files\Missing_dlls_v1.0.7z /E /H /C /I
 ) ELSE (
     ECHO Missing_dlls file already exists.
 )
@@ -23,7 +23,7 @@ IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\Missing_dlls.7z" (
 
 color 1f
 echo *** Extracting ...***
-D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\Files\Missing_dlls.7z -oD:\Ajay_prefix\wget_files\temp\ -r -y >NUL 2>&1
+D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\Files\Missing_dlls_v1.0.7z -oD:\Ajay_prefix\wget_files\temp\ -p-q -r -y >NUL 2>&1
 echo *** installing...***
 Xcopy /e /y /d D:\Ajay_prefix\wget_files\temp\system32\ C:\windows\system32\ /E /H /C /I
 Xcopy /e /y /d D:\Ajay_prefix\wget_files\temp\syswow64\ C:\windows\syswow64\ /E /H /C /I
