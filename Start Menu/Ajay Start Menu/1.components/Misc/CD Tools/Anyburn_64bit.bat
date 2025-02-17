df2533a1cf451d9cfdfd812bbc3090ac1226cc3c
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-02-17 07:36:13 +0530

    Add files via upload

---
+++ Start Menu/Ajay Start Menu/1.components/Misc/CD Tools/Anyburn_64bit.bat
@@ -0,0 +1,23 @@
+@echo off
+color 0a
+echo *** script made by Ajay ***
+
+:: Simulating bold with color and emphasis
+color 0A
+echo.
+echo *** Downloading AnyBurn ***
+IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\AnyBurn.7z" (
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/AnyBurn.7z
+    copy /s /y D:\Ajay_prefix\wget_files\temp\AnyBurn.7z D:\Ajay_prefix\wget_files\Files\AnyBurn.7z /E /H /C /I
+) ELSE (
+    ECHO AnyBurn already exists.
+)
+
+
+color 1f
+echo *** Extracting ...***
+D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\Files\AnyBurn.7z -oD:\Ajay_prefix\wget_files\my_apps\ -p-q -r -y >NUL 2>&1
+
+echo *** opening AnyBurn ***
+Start D:\Ajay_prefix\wget_files\my_apps\AnyBurn\AnyBurn.exe
+echo ************************************************
