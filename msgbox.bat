b309f6eed66eed5cf9ca99d11733ee6e68fb6052
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-02-17 07:56:41 +0530

    Update msgbox.bat

--- msgbox.bat
+++ msgbox.bat
@@ -68,8 +68,13 @@ mkdir D:\Ajay_prefix\save_data\users\xuser\AppData\Roaming 2>nul
 mkdir D:\Ajay_prefix\save_data\users\xuser\Documents 2>nul
 mkdir "D:\Ajay_prefix\save_data\users\xuser\Documents\My Games" 2>nul
 mkdir "D:\Ajay_prefix\save_data\users\xuser\"Saved Games 2>nul
-
+cls
+echo *** deleting temp files...***
+rmdir /S /Q "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
+mkdir "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
+echo *** deleted temp files ***
 echo msgbox "Hello! Ajay Prefix components are updated successfully, Changelog and Files are saved to drive_D/Ajay_prefix/wget_files/ !" , vbinformation+vbSystemModal > %tmp%\tmp.vbs
 cscript /nologo %tmp%\tmp.vbs
 del %tmp%\tmp.vbs
 rmdir /S /Q "C:/windows/temp" >NUL 2>&1
+
