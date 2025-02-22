678964cd1a391a1da5da05175902897dc04f9575
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-02-22 09:34:26 +0530

    Update install multilingual Fonts.bat
    Added gulim fonts for Korean

--- Start Menu/Ajay Start Menu/1.components/Fixes/Fonts/install multilingual Fonts.bat
+++ Start Menu/Ajay Start Menu/1.components/Fixes/Fonts/install multilingual Fonts.bat
@@ -29,10 +29,24 @@ IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\Fonts.part2.rar" (
     ECHO Part 02 already exists.
 )
 
+color 0A
+echo.
+echo *** Downloading Fonts addon ***
+IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\Fonts_addon.7z" (
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Fonts_addon.7z
+    copy /s /y D:\Ajay_prefix\wget_files\temp\Fonts_addon.7z D:\Ajay_prefix\wget_files\Files\Fonts_addon.7z /E /H /C /I
+) ELSE (
+    ECHO fonts addon already exists.
+)
+
 color 1f
 echo Extracting...
 D:\Ajay_prefix\.Resources\winrar.exe x D:\Ajay_prefix\wget_files\Files\Fonts.part1.rar D:\Ajay_prefix\wget_files\temp\
 echo.
-echo *** installing to C drive  ***
+echo *** installing multilingual Fonts to C drive  ***
 D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\temp\Fonts.7z -oC:\windows\ -r -y >NUL 2>&1
+
+echo *** installing multilingual Fonts to C drive  ***
+D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\Files\Fonts_addon.7z -oC:\windows\ -r -y >NUL 2>&1
+
 timeout.exe 5 /nobreak
