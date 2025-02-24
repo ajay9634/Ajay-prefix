f57e2eeeb73aa21befbb6946ae7f54fc85555437
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-02-24 08:19:24 +0530

    Delete Start Menu/Ajay Start Menu/8.Backup and Restore drive C users and ProgramData directory

--- Start Menu/Ajay Start Menu/8.Backup and Restore drive C users and ProgramData/Restore drive C data.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-color 84
-echo Restoring...
-D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\save_data\drive_c.7z -oC:\ -p-q -r -y >NUL 2>&1
-echo msgbox "Hello! some Important Game save folders from 'Drive-D/Ajay_prefix/save_data/drive_c.7z ' are Restored to 'drive-C' ! You can Backup it in any container by 'Backup Drive C users and ProgramData.bat'" , vbinformation+vbSystemModal > %tmp%\tmp.vbs
-cscript /nologo %tmp%\tmp.vbs
-del %tmp%\tmp.vbs
