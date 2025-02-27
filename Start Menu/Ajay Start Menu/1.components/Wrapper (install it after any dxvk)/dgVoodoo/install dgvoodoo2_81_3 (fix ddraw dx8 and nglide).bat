2528c81da64d6b3f4fa95d6bdd92038ffad37afe
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-02-27 11:30:17 +0530

    Update install dgvoodoo2_81_3 (fix ddraw dx8 and nglide).bat

--- Start Menu/Ajay Start Menu/1.components/Wrapper (install it after any dxvk)/dgVoodoo/install dgvoodoo2_81_3 (fix ddraw dx8 and nglide).bat
+++ Start Menu/Ajay Start Menu/1.components/Wrapper (install it after any dxvk)/dgVoodoo/install dgvoodoo2_81_3 (fix ddraw dx8 and nglide).bat
@@ -27,6 +27,7 @@ echo *** Extracting ...***
 D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\d3d\%installname%.7z -oD:\Ajay_prefix\wget_files\temp\ -p-q -r -y >NUL 2>&1
 echo *** installing...***
 Xcopy /s /y D:\Ajay_prefix\wget_files\temp\syswow64\ C:\windows\syswow64\ /E /H /C /I
+Xcopy /s /y D:\Ajay_prefix\wget_files\temp\system32\ C:\windows\system32\ /E /H /C /I
 echo ************************************************
 echo Note - nglide dlls are replaced by dgvoodoo. you need to install nglide again to use it. you should delete all nglide related dlls from the game folder for dgvoodoo effect.
 
