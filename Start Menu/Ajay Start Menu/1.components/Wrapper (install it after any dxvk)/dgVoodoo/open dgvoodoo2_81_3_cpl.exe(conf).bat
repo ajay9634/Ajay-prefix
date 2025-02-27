196cc353b732882ca5d55c80c16bc260dd5bdcd9
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-02-27 13:13:53 +0530

    Add files via upload

---
+++ Start Menu/Ajay Start Menu/1.components/Wrapper (install it after any dxvk)/dgVoodoo/open dgvoodoo2_81_3_cpl.exe(conf).bat
@@ -0,0 +1,39 @@
+@echo off
+set "installname=dgVoodoo2_81_3_cpl"
+:: Remove the prefix "dxvk-" and keep the part after it
+color 0a
+echo *** wait....***
+echo *** deleting temp files and old dgvoodoo conf ...***
+rmdir /S /Q "D:/Ajay_prefix/wget_files/temp"
+mkdir "D:/Ajay_prefix/wget_files/temp"
+del "C:\windows\system32\dgVoodooCpl.exe" >NUL 2>&1
+del "C:\windows\syswow64\dgVoodooCpl.exe" >NUL 2>&1
+del "C:\windows\syswow64\dgVoodoo.conf" >NUL 2>&1
+del "C:\windows\system32\dgVoodoo.conf" >NUL 2>&1
+echo *** deleted temp files ***
+
+echo .
+echo *** script made by Ajay ***
+
+:: Simulating bold with color and emphasis
+color 0A
+echo.
+echo *** Downloading %installname%  ***
+IF NOT EXIST "D:\Ajay_prefix\wget_files\d3d\%installname%.7z" (
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/%installname%.7z
+    copy /s /y D:\Ajay_prefix\wget_files\temp\%installname%.7z D:\Ajay_prefix\wget_files\d3d\%installname%.7z /E /H /C /I
+) ELSE (
+    ECHO %installname% file already exists.
+)
+
+:: Simulate download progress only for subsequent parts
+color 1f
+echo *** Extracting ...***
+D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\d3d\%installname%.7z -oD:\Ajay_prefix\wget_files\temp\ -p-q -r -y >NUL 2>&1
+echo *** installing...***
+Xcopy /s /y D:\Ajay_prefix\wget_files\temp\system32\ C:\windows\system32\ /E /H /C /I
+
+echo opening dgVoodoocpl.exe ...
+timeout.exe 3 /nobreak >NUL 2>&1
+Start /wait C:\windows\system32\dgVoodooCpl.exe
+pause
