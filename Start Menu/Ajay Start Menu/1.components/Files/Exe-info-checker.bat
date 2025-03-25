1993fa90170c18d99689f1cddb7726c91b52965c
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-03-26 00:28:35 +0530

    Exe info checker
    Made by me

---
+++ Start Menu/Ajay Start Menu/1.components/Files/Exe-info-checker.bat
@@ -0,0 +1,35 @@
+@echo off
+color 0a
+echo *** You can check exe info in details  ***
+echo *** Script made by Ajay ***
+
+:: Downloading NSIS-3.11 if it doesn't exist
+echo *** Checking for Exe-info-checker ***
+IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\Exe-info-checker.7z" (
+    echo Downloading NSIS-3.11...
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Exe-info-checker.7z
+    copy /y D:\Ajay_prefix\wget_files\temp\Exe-info-checker.7z D:\Ajay_prefix\wget_files\Files\
+) ELSE (
+    echo Exe-info-checker already exists.
+)
+
+:: Extracting NSIS
+color 1f
+echo *** Extracting Exe-info-checker ***
+D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\Files\Exe-info-checker.7z -oD:\Ajay_prefix\wget_files\temp\ -p-q -r -y >NUL 2>&1
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo Extraction failed. Please check the file and try again.
+    pause
+    exit /b
+)
+
+:: Launching the Installer
+echo *** Opening Exe-info-checker  ***
+echo msgbox "Hello! Extract it in game exe or app exe folder !" , vbinformation+vbSystemModal > %tmp%\tmp.vbs
+start /b cscript //nologo %tmp%\tmp.vbs
+timeout.exe /t 1 >nul
+del %tmp%\tmp.vbs
+Start "" D:\Ajay_prefix\wget_files\temp\Exe-info-checker.exe
+
+echo ************************************************
