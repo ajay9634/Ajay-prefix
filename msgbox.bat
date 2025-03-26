fa873c8b4b2388cee968b10af51eceb5c8fd7e29
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-03-26 08:23:54 +0530

    Update msgbox.bat

--- msgbox.bat
+++ msgbox.bat
@@ -78,5 +78,8 @@ rmdir /S /Q "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
 mkdir "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
 echo msgbox "Hello! Ajay Prefix components are updated successfully, Changelog and Files are saved to drive_D/Ajay_prefix/wget_files/!" , vbinformation+vbSystemModal > %tmp%\tmp.vbs
 start /b cscript //nologo %tmp%\tmp.vbs
-timeout.exe /t 1 >nul
-del %tmp%\tmp.vbs
+timeout.exe /t 2 >nul
+del %tmp%\tmp.vbs 
+del C:/msgbox.bat >nul 2>&1
+del C:/ajay_prefix.reg >nul 2>&1
+del C:/prefix.bat >nul 2>&1
