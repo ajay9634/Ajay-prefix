ed5db169199f55546f9befde1515ca1efe040943
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-03-26 06:58:18 +0530

    Update nsis-3.11(SFX maker).bat

--- Start Menu/Ajay Start Menu/1.components/Files/nsis-3.11(SFX maker).bat
+++ Start Menu/Ajay Start Menu/1.components/Files/nsis-3.11(SFX maker).bat
@@ -6,11 +6,11 @@ echo *** Script made by Ajay ***
 :: Downloading NSIS-3.11 if it doesn't exist
 echo *** Checking for NSIS-3.11 Setup ***
 IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\nsis-3.11-setup.7z" (
-    echo Downloading NSIS-3.11...
+    echo *** Downloading NSIS-3.11...***
     wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/nsis-3.11-setup.7z
     copy /y D:\Ajay_prefix\wget_files\temp\nsis-3.11-setup.7z D:\Ajay_prefix\wget_files\Files\
 ) ELSE (
-    echo NSIS-3.11 setup already exists.
+    echo *** NSIS-3.11 setup already exists.***
 )
 
 :: Extracting NSIS
@@ -19,7 +19,7 @@ echo *** Extracting NSIS-3.11 ***
 D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\Files\nsis-3.11-setup.7z -oD:\Ajay_prefix\wget_files\temp\ -r -y >NUL 2>&1
 
 IF %ERRORLEVEL% NEQ 0 (
-    echo Extraction failed. Please check the file and try again.
+    echo *** Extraction failed. Please check the file and try again.***
     pause
     exit /b
 )
