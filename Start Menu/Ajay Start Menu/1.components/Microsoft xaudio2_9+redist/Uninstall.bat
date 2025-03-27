bec2f035ccb872d6952e4be19c071fe0282e3d6a
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-03-27 16:12:42 +0530

    Update Uninstall.bat

--- Start Menu/Ajay Start Menu/1.components/Microsoft xaudio2_9+redist/Uninstall.bat
+++ Start Menu/Ajay Start Menu/1.components/Microsoft xaudio2_9+redist/Uninstall.bat
@@ -1,15 +1,24 @@
 @echo off
+color 0c
 echo Uninstalling Microsoft-xaudio2_9+redist ...
-del C:\temp\Resources\fix\xaudio2_9\system32\xaudio2_9redist.dll >NUL 2>&1
+
+:: Delete DLL files silently
+del C:\windows\syswow64\xaudio2_9redist.dll >NUL 2>&1
 del C:\windows\system32\xaudio2_9redist.dll >NUL 2>&1
-del C:\temp\Resources\fix\xaudio2_9\system32\xaudio2_9.dll >NUL 2>&1
+del C:\windows\syswow64\xaudio2_9.dll >NUL 2>&1
 del C:\windows\system32\xaudio2_9.dll >NUL 2>&1
 
-echo updating registry
-reg add "HKCU\Software\Wine\DllOverrides" /v "xaudio2_9" /t REG_SZ /d "builtin,native" /f >nul 2>&1
-reg add "HKCU\Software\Wine\DllOverrides" /v "xaudio2_9redist" /t REG_SZ /d "builtin,native" /f >nul 2>&1
+echo Updating registry...
+reg add "HKCU\Software\Wine\DllOverrides" /v "xaudio2_9" /t REG_SZ /d "builtin,native" /f >NUL 2>&1
+reg add "HKCU\Software\Wine\DllOverrides" /v "xaudio2_9redist" /t REG_SZ /d "builtin,native" /f >NUL 2>&1
+
+:: Add a short delay
 timeout.exe 5 /nobreak >NUL 2>&1
-reg add "HKCU\Software\Wine\DllOverrides" /v "xaudio2_9" /t REG_SZ /d "builtin,native" /f >nul 2>&1
-reg add "HKCU\Software\Wine\DllOverrides" /v "xaudio2_9redist" /t REG_SZ /d "builtin,native" /f >nul 2>&1
-echo Uninstalled !
+
+:: Ensure registry changes are applied
+reg add "HKCU\Software\Wine\DllOverrides" /v "xaudio2_9" /t REG_SZ /d "builtin,native" /f >NUL 2>&1
+reg add "HKCU\Software\Wine\DllOverrides" /v "xaudio2_9redist" /t REG_SZ /d "builtin,native" /f >NUL 2>&1
+
+color 0a
+echo Uninstalled Successfully!
 timeout.exe 3 /nobreak >NUL 2>&1
