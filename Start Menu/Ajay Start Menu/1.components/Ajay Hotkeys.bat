eb7c308fcd437543e2cc2f97ab4bb1957079a87e
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-04-02 09:42:15 +0530

    Update Ajay Hotkeys.bat

--- Start Menu/Ajay Start Menu/1.components/Ajay Hotkeys.bat
+++ Start Menu/Ajay Start Menu/1.components/Ajay Hotkeys.bat
@@ -4,10 +4,10 @@ echo *** Script made by Ajay ***
 
 :: Downloading Ajay_Hotkeys if it doesn't exist
 echo *** Checking for Ajay_Hotkeys ***
-IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\Ajay_Hotkeys.7z" (
+IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\Ajay_Hotkeys_v1.7z" (
     echo *** Downloading Ajay_Hotkeys...***
-    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Ajay_Hotkeys.7z
-    copy /y D:\Ajay_prefix\wget_files\temp\Ajay_Hotkeys.7z D:\Ajay_prefix\wget_files\Files\
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Ajay_Hotkeys_v1.7z
+    copy /y D:\Ajay_prefix\wget_files\temp\Ajay_Hotkeys_v1.7z D:\Ajay_prefix\wget_files\Files\
 ) ELSE (
     echo *** Ajay_Hotkeys already exists ***
 )
@@ -15,7 +15,7 @@ IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\Ajay_Hotkeys.7z" (
 :: Extracting NSIS
 color 1f
 echo *** Extracting Ajay_Hotkeys ***
-D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\Files\Ajay_Hotkeys.7z -oD:\Ajay_prefix\wget_files\temp\ -p-q -r -y >NUL 2>&1
+D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\Files\Ajay_Hotkeys_v1.7z -oD:\Ajay_prefix\wget_files\temp\ -p-q -r -y >NUL 2>&1
 
 IF %ERRORLEVEL% NEQ 0 (
     echo *** Extraction failed. Please check the file and try again ***
