d1d5d9da555b9ea6d5d629a1cd67c2b199911ceb
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-04-13 23:16:59 +0530

    Update dxvk custom version(old and latest).bat

--- Start Menu/Ajay Start Menu/1.components/Dxvk/dxvk custom version(old and latest).bat
+++ Start Menu/Ajay Start Menu/1.components/Dxvk/dxvk custom version(old and latest).bat
@@ -1,21 +1,30 @@
 @echo off
 setlocal EnableDelayedExpansion
-echo ( Recommended old dxvk version :- 0.40 , 0.54 , 0.96 , 1.5.5 , 1.7.1 , 1.7.3 , 1.10 , 1.10.3 , 2.0 , 2.1 , 2.3.1 , 2.4.1 ) 
-goto show_version_list
-:version_input
-set /p "version=Type the version [eg 2.4.1]: "
-if "%version%"=="" set "version=%latest_version%"
-
-echo You entered version: %version%
-echo.
-color 0a
-echo *** wait....***
 echo *** deleting temp files...***
 rmdir /S /Q "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
 rmdir /S /Q "C:/windows/temp" >NUL 2>&1
 mkdir "C:/windows/temp" >NUL 2>&1
 mkdir "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
 echo *** deleted temp files ***
+cls
+echo ( Recommended old dxvk version :- 0.40 , 0.54 , 0.96 , 1.5.5 , 1.7.1 , 1.7.3 , 1.10 , 1.10.3 , 2.0 , 2.1 , 2.3.1 , 2.4.1 )
+echo.
+echo Fetching last 30 available DXVK versions...
+:: Ensure temp folder exists before download
+if not exist "D:/Ajay_prefix/wget_files/temp" mkdir "D:/Ajay_prefix/wget_files/temp"
+
+:: Download the list of releases
+wget -q -O D:/Ajay_prefix/wget_files/temp/all_releases.json https://api.github.com/repos/doitsujin/dxvk/releases
+
+goto show_version_list
+
+:version_input
+set /p "version=Type the version [eg 2.4.1]: "
+echo.
+echo You entered version: %version%
+echo.
+color 0a
+echo *** wait....***
 echo .
 echo *** script made by Ajay ***
 
@@ -40,16 +49,14 @@ Xcopy /s /y C:\windows\temp\dxvk-%version%\system32\ C:\windows\system32\ /E /H 
 Xcopy /s /y C:\windows\temp\dxvk-%version%\syswow64\ C:\windows\syswow64\ /E /H /C /I
 
 echo ************************************************
-timeout.exe 5 /nobreak
+timeout.exe 3 /nobreak
 goto end
 
 :show_version_list
 echo.
-echo Fetching last 30 available DXVK versions...
-echo.
 setlocal EnableDelayedExpansion
 set count=0
-for /f "tokens=2 delims=:" %%A in ('findstr /i "tag_name" all_releases.json 2^>nul') do (
+for /f "tokens=2 delims=:" %%A in ('findstr /i "tag_name" D:/Ajay_prefix/wget_files/temp/all_releases.json 2^>nul') do (
     set "ver=%%A"
     set "ver=!ver:,=!"
     set "ver=!ver:"=!"
@@ -60,8 +67,6 @@ for /f "tokens=2 delims=:" %%A in ('findstr /i "tag_name" all_releases.json 2^>n
 )
 endlocal
 echo.
-timeout.exe 5 /nobreak
 goto version_input
 
 :end
-del all_releases.json >nul 2>&1
