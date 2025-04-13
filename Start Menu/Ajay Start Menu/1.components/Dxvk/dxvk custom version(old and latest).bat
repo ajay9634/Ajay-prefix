2ffe85f48baa808cfee7ef5f25573f78302e5c63
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-04-13 22:44:46 +0530

    Update dxvk custom version(old and latest).bat

--- Start Menu/Ajay Start Menu/1.components/Dxvk/dxvk custom version(old and latest).bat
+++ Start Menu/Ajay Start Menu/1.components/Dxvk/dxvk custom version(old and latest).bat
@@ -1,28 +1,9 @@
 @echo off
 setlocal EnableDelayedExpansion
-echo Checking Latest available DXVK official version ...
-echo.
-
-:: Download the latest release info from GitHub using wget
-wget -q -O latest.json https://api.github.com/repos/doitsujin/dxvk/releases/latest
-
-:: Extract the version tag using findstr and for loop
-set "latest_version="
-for /f "tokens=2 delims=:" %%A in ('findstr /i "tag_name" latest.json 2^>nul') do (
-    set "ver=%%A"
-    set "ver=!ver:,=!"
-    set "ver=!ver:"=!"
-    set "ver=!ver: =!"
-    set "latest_version=!ver:v=!"
-)
-
-del latest.json >nul 2>&1
-
-echo Latest available DXVK version is: %latest_version%
-echo.
-
+echo ( Recommended old dxvk version :- 0.40 , 0.54 , 0.96 , 1.5.5 , 1.7.1 , 1.7.3 , 1.10 , 1.10.3 , 2.0 , 2.1 , 2.3.1 , 2.4.1 ) 
+goto show_version_list
 :version_input
-set /p "version=Type the version [default: %latest_version%]: "
+set /p "version=Type the version [eg 2.4.1]: "
 if "%version%"=="" set "version=%latest_version%"
 
 echo You entered version: %version%
@@ -53,10 +34,33 @@ color 1f
 echo *** Extracting ...***
 D:\Ajay_prefix\.Resources\winrar.exe x D:\Ajay_prefix\wget_files\d3d\dxvk-%version%.tar.gz C:\windows\temp\ -r -y >NUL 2>&1
 echo *** installing...***
-ren "C:\windows\temp\dxvk-%version%\x64" system32
-ren "C:\windows\temp\dxvk-%version%\x32" syswow64
+if exist "C:\windows\temp\dxvk-%version%\x64" ren "C:\windows\temp\dxvk-%version%\x64" system32
+if exist "C:\windows\temp\dxvk-%version%\x32" ren "C:\windows\temp\dxvk-%version%\x32" syswow64
 Xcopy /s /y C:\windows\temp\dxvk-%version%\system32\ C:\windows\system32\ /E /H /C /I
 Xcopy /s /y C:\windows\temp\dxvk-%version%\syswow64\ C:\windows\syswow64\ /E /H /C /I
 
 echo ************************************************
 timeout.exe 5 /nobreak
+goto end
+
+:show_version_list
+echo.
+echo Fetching last 30 available DXVK versions...
+echo.
+setlocal EnableDelayedExpansion
+set count=0
+for /f "tokens=2 delims=:" %%A in ('findstr /i "tag_name" all_releases.json 2^>nul') do (
+    set "ver=%%A"
+    set "ver=!ver:,=!"
+    set "ver=!ver:"=!"
+    set "ver=!ver: =!"
+    set "ver=!ver:v=!"
+    set /a count+=1
+    echo !count!. dxvk-!ver!
+)
+endlocal
+echo.
+goto version_input
+
+:end
+del all_releases.json >nul 2>&1
