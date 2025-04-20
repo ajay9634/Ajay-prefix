1750b058d10e244bf8c2777245abf03e8cfe7e30
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-04-20 14:06:58 +0530

    Update Delete temp files.bat

--- Start Menu/Ajay Start Menu/1.components/Microsoft Samples/Delete temp files.bat
+++ Start Menu/Ajay Start Menu/1.components/Microsoft Samples/Delete temp files.bat
@@ -1,6 +1,7 @@
 @echo off
 echo *** deleting temp files...***
 rmdir /S /Q "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
+rmdir /S /Q "D:/Ajay_prefix/wget_files/temp2" >NUL 2>&1
 rmdir /S /Q "C:/windows/temp" >NUL 2>&1
 mkdir "C:/windows/temp" >NUL 2>&1
 mkdir "D:/Ajay_prefix/wget_files/temp" >NUL 2>&
