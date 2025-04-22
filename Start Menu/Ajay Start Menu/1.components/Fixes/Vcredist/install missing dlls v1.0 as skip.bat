bc2ec7e8d77ee9ce36bbd7a177e9ecf8ecd54643
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-04-22 21:24:37 +0530

    Update install missing dlls v1.0 as skip.bat

--- Start Menu/Ajay Start Menu/1.components/Fixes/Vcredist/install missing dlls v1.0 as skip.bat
+++ Start Menu/Ajay Start Menu/1.components/Fixes/Vcredist/install missing dlls v1.0 as skip.bat
@@ -6,7 +6,7 @@ rmdir /S /Q "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
 mkdir "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
 del D:\Ajay_prefix\wget_files\Files\Missing_dlls.7z >NUL 2>&1
 echo *** deleted temp files ***
-echo .
+echo.
 echo *** script made by Ajay ***
 
 :: Simulating bold with color and emphasis
