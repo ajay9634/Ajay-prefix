d25ad9d85e1aba304fd3bc84fb5973893814fb5a
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-04-23 09:57:29 +0530

    Add files via upload

---
+++ Start Menu/Ajay Start Menu/1.components/Files/7z24.09-zstd-x64.bat
@@ -0,0 +1,26 @@
+@echo off
+color 0a
+echo *** Easily extract and edit .zst, .zip, .7z, and other archive formats with 7z24.09-zstd-x64 ***
+echo.
+echo *** script made by Ajay ***
+
+:: Simulating bold with color and emphasis
+color 0A
+echo.
+echo *** Downloading 7z24.09-zstd-x64 ***
+IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\7z24.09-zstd-x64.7z" (
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/Archive-Tools/7z24.09-zstd-x64.7z
+    copy /s /y D:\Ajay_prefix\wget_files\temp\7z24.09-zstd-x64.7z D:\Ajay_prefix\wget_files\Files\7z24.09-zstd-x64.7z /E /H /C /I
+) ELSE (
+    ECHO File already exists.
+)
+
+
+color 1f
+echo *** Extracting ...***
+D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\Files\7z24.09-zstd-x64.7z -o"C:\Program Files\" -p-q -r -y >NUL 2>&1
+
+echo *** Opening 7z24.09-zstd-x64  ***
+Start "7zFM" "C:\Program Files\7z24.09-zstd-x64\7zFM.exe"
+echo ************************************************
+timeout.exe /t 3 >nul
