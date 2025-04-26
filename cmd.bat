7df4edf6885b33e44dc34c5157e36dd24f1eb15d
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-04-26 23:49:35 +0530

    Update cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -67,6 +67,8 @@ reg add "HKCR\.html" /ve /d "txtfile" /f >nul
 reg add "HKCR\.htm" /ve /d "txtfile" /f >nul
 reg add "HKCR\.css" /ve /d "txtfile" /f >nul
 reg add "HKCR\.js" /ve /d "txtfile" /f >nul
+reg add "HKCR\.desktop" /ve /d "txtfile" /f >nul
+
 
 reg add "HKCR\txtfile" /ve /d "Text Document" /f >nul
 reg add "HKCR\txtfile\DefaultIcon" /ve /d "C:\windows\system32\imageres.dll,-102" /f >nul
