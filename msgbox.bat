87b9d1c0c74b33881afdb184d0fe8e8f175ef224
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-04-26 12:40:50 +0530

    Update msgbox.bat

--- msgbox.bat
+++ msgbox.bat
@@ -133,6 +133,28 @@ reg add "HKCR\.tlz" /ve /d "7-Zip.archive" /f >nul 2>&1
 reg add "HKCR\.iso" /ve /d "7-Zip.archive" /f >nul 2>&1
 reg add "HKCR\.rar" /ve /d "7-Zip.archive" /f >nul 2>&1
 reg add "HKCR\.zip" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.cab" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.deb" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.wcp" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.zip" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.bzip2" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.gzip" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.lzma" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.rpm" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.gz" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.bz2" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.swm" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.esd" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.ar" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.cpio" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.dmg" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.lzh" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.vdi" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.vhd" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.vmdk" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.z" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.udf" /ve /d "7-Zip.archive" /f >nul 2>&1
+reg add "HKCR\.hfs" /ve /d "7-Zip.archive" /f >nul 2>&1
 
 :: 7-Zip archive type
 reg add "HKCR\7-Zip.archive" /ve /d "Compressed Archive" /f >nul 2>&1
