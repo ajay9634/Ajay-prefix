89406e2b982d87861e6349390094cb7ac4e72c26
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-04-26 12:27:42 +0530

    Update msgbox.bat

--- msgbox.bat
+++ msgbox.bat
@@ -130,24 +130,14 @@ reg add "HKCR\.lzma" /ve /d "7-Zip.archive" /f >nul 2>&1
 reg add "HKCR\.tgz" /ve /d "7-Zip.archive" /f >nul 2>&1
 reg add "HKCR\.tbz2" /ve /d "7-Zip.archive" /f >nul 2>&1
 reg add "HKCR\.tlz" /ve /d "7-Zip.archive" /f >nul 2>&1
-reg add "HKCR\.iso" /ve /d "7-Zip.iso" /f >nul 2>&1
+reg add "HKCR\.iso" /ve /d "7-Zip.archive" /f >nul 2>&1
 reg add "HKCR\.rar" /ve /d "7-Zip.archive" /f >nul 2>&1
-reg add "HKCR\.zip" /ve /d "ZIP.archive" /f >nul 2>&1
+reg add "HKCR\.zip" /ve /d "7-Zip.archive" /f >nul 2>&1
 
 :: 7-Zip archive type
 reg add "HKCR\7-Zip.archive" /ve /d "Compressed Archive" /f >nul 2>&1
 reg add "HKCR\7-Zip.archive\DefaultIcon" /ve /d "C:\\windows\\7z.dll,2" /f >nul 2>&1
 reg add "HKCR\7-Zip.archive\Shell\Open\Command" /ve /d "\"C:\\windows\\7zFM.exe\" \"%%1\"" /f >nul 2>&1
-
-:: 7-Zip ISO type
-reg add "HKCR\7-Zip.iso" /ve /d "ISO Disk Image" /f >nul 2>&1
-reg add "HKCR\7-Zip.iso\DefaultIcon" /ve /d "C:\\windows\\7z.dll,8" /f >nul 2>&1
-reg add "HKCR\7-Zip.iso\Shell\Open\Command" /ve /d "\"C:\\windows\\7zFM.exe\" \"%%1\"" /f >nul 2>&1
-
-:: ZIP archive using Windows handler
-reg add "HKCR\ZIP.archive" /ve /d "ZIP Archive" /f >nul 2>&1
-reg add "HKCR\ZIP.archive\DefaultIcon" /ve /d "C:\\windows\\System32\\zipfldr.dll,0" /f >nul 2>&1
-reg add "HKCR\ZIP.archive\Shell\Open\Command" /ve /d "C:\\windows\\System32\\cmd.exe /c start zipfldr.dll \"%%1\"" /f >nul 2>&1
 
 reg add "HKCU\Software\7-Zip\FM" /v ShowRealFileIcons /t REG_DWORD /d 1 /f >nul 2>&1
 
