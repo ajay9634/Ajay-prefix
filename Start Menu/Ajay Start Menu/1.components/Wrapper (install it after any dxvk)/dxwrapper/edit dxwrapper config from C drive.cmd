9affc56f76dcb4a58982ff537052ecd99a8568c8
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-04-30 13:42:22 +0530

    Update edit dxwrapper config from C drive.cmd

--- Start Menu/Ajay Start Menu/1.components/Wrapper (install it after any dxvk)/dxwrapper/edit dxwrapper config from C drive.cmd
+++ Start Menu/Ajay Start Menu/1.components/Wrapper (install it after any dxvk)/dxwrapper/edit dxwrapper config from C drive.cmd
@@ -1,6 +1,6 @@
 @echo off
 if exist "C:\windows\syswow64\dxwrapper.ini" (
-    notepad.exe "C:\windows\syswow64\dxwrapper.ini"
+    Start "" notepad.exe "C:\windows\syswow64\dxwrapper.ini"
 ) else (
     echo Config doesn't exist, install dxwrapper first.
     pause
