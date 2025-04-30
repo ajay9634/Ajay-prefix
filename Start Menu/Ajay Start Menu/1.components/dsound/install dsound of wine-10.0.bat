5d45a4e5a6f7506a143f90baa8d458ef0e70b944
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-04-30 08:15:46 +0530

    Add files via upload

---
+++ Start Menu/Ajay Start Menu/1.components/dsound/install dsound of wine-10.0.bat
@@ -0,0 +1,30 @@
+@echo off
+color 0a
+echo *** wait....***
+echo *** deleting temp files...***
+rmdir /S /Q "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
+mkdir "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
+echo *** deleted temp files ***
+echo.
+echo *** script made by Ajay ***
+
+:: Simulating bold with color and emphasis
+color 0A
+echo.
+echo *** Downloading dsound-10.0 ***
+IF NOT EXIST "D:\Ajay_prefix\wget_files\dsound\dsound-10.0.7z" (
+    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/dsound-10.0.7z
+    copy /s /y D:\Ajay_prefix\wget_files\temp\dsound-10.0.7z D:\Ajay_prefix\wget_files\dsound\dsound-10.0.7z /E /H /C /I
+) ELSE (
+    ECHO dsound-10.0 file already exists.
+)
+
+color 1f
+echo *** Extracting ...***
+D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\dsound\dsound-10.0.7z -oD:\Ajay_prefix\wget_files\temp\ -r -y >NUL 2>&1
+echo *** installing...***
+Xcopy /s /y D:\Ajay_prefix\wget_files\temp\system32\ C:\windows\system32\ /E /H /C /I
+Xcopy /s /y D:\Ajay_prefix\wget_files\temp\syswow64\ C:\windows\syswow64\ /E /H /C /I
+
+echo ************************************************
+timeout.exe 5 /nobreak
