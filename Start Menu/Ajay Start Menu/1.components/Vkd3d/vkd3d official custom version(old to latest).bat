307267af2e81e4e83bb2b5adcfa3a1de5d9ceeec
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-05 11:10:04 +0530

    Add files via upload

--- Start Menu/Ajay Start Menu/1.components/Vkd3d/vkd3d official custom version(old to latest).bat
+++ Start Menu/Ajay Start Menu/1.components/Vkd3d/vkd3d official custom version(old to latest).bat
@@ -1,8 +1,10 @@
 @echo off
+call "C:\Windows\Ajay_drive.bat" >nul 2>&1
+if not defined drive_letter set drive_letter=D
 setlocal EnableDelayedExpansion
 echo *** Deleting temp files... ***
-rmdir /S /Q "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
-mkdir "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
+rmdir /S /Q "%drive_letter%:/Ajay_prefix/wget_files/temp" >NUL 2>&1
+mkdir "%drive_letter%:/Ajay_prefix/wget_files/temp" >NUL 2>&1
 rmdir /S /Q "C:/windows/temp" >NUL 2>&1
 mkdir "C:/windows/temp" >NUL 2>&1
 echo *** Temp cleaned ***
@@ -10,7 +12,7 @@ cls
 
 echo ( Recommended versions: 2.14.1 , 2.13 , 2.12 , 2.11 , 2.10 , 2.9 , 2.8 )
 echo Fetching available VKD3D-Proton versions...
-wget -q -O D:/Ajay_prefix/wget_files/temp/all_releases.json https://api.github.com/repos/HansKristian-Work/vkd3d-proton/releases
+wget -q -O %drive_letter%:/Ajay_prefix/wget_files/temp/all_releases.json https://api.github.com/repos/HansKristian-Work/vkd3d-proton/releases
 
 goto show_version_list
 
@@ -27,10 +29,10 @@ echo *** script made by Ajay ***
 color 0A
 echo.
 echo *** Downloading vkd3d-proton-%version% ***
-IF NOT EXIST "D:\Ajay_prefix\wget_files\d3d\vkd3d-proton-%version%.tar.zst" (
-    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://github.com/HansKristian-Work/vkd3d-proton/releases/download/v%version%/vkd3d-proton-%version%.tar.zst
+IF NOT EXIST "%drive_letter%:\Ajay_prefix\wget_files\d3d\vkd3d-proton-%version%.tar.zst" (
+    wget -q --show-progress -P %drive_letter%:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://github.com/HansKristian-Work/vkd3d-proton/releases/download/v%version%/vkd3d-proton-%version%.tar.zst
 
-    copy /y D:\Ajay_prefix\wget_files\temp\vkd3d-proton-%version%.tar.zst D:\Ajay_prefix\wget_files\d3d\vkd3d-proton-%version%.tar.zst
+    copy /y %drive_letter%:\Ajay_prefix\wget_files\temp\vkd3d-proton-%version%.tar.zst %drive_letter%:\Ajay_prefix\wget_files\d3d\vkd3d-proton-%version%.tar.zst
 ) ELSE (
     ECHO vkd3d-proton-%version%.tar.zst already exists.
 )
@@ -38,17 +40,17 @@ IF NOT EXIST "D:\Ajay_prefix\wget_files\d3d\vkd3d-proton-%version%.tar.zst" (
 color 1f
 echo *** Decompressing and Extracting... ***
 
-IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\7z24.09-zstd-x64.7z" (
-    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/Archive-Tools/7z24.09-zstd-x64.7z
-    copy /s /y D:\Ajay_prefix\wget_files\temp\7z24.09-zstd-x64.7z D:\Ajay_prefix\wget_files\Files\7z24.09-zstd-x64.7z /E /H /C /I
+IF NOT EXIST "%drive_letter%:\Ajay_prefix\wget_files\Files\7z24.09-zstd-x64.7z" (
+    wget -q --show-progress -P %drive_letter%:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/Archive-Tools/7z24.09-zstd-x64.7z
+    copy /s /y %drive_letter%:\Ajay_prefix\wget_files\temp\7z24.09-zstd-x64.7z %drive_letter%:\Ajay_prefix\wget_files\Files\7z24.09-zstd-x64.7z /E /H /C /I
 ) ELSE (
     ECHO 7z24.09-zstd-x64 already exists.
 )
 
-D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\Files\7z24.09-zstd-x64.7z -o"C:\Program Files\" -p-q -r -y >NUL 2>&1
+%drive_letter%:\Ajay_prefix\.Resources\7z.exe x %drive_letter%:\Ajay_prefix\wget_files\Files\7z24.09-zstd-x64.7z -o"C:\Program Files\" -p-q -r -y >NUL 2>&1
 
 :: rem Extract .tar.zst
-"C:\Program Files\7z24.09-zstd-x64\7z.exe" x "D:\Ajay_prefix\wget_files\d3d\vkd3d-proton-%version%.tar.zst" -o"C:\windows\temp"
+"C:\Program Files\7z24.09-zstd-x64\7z.exe" x "%drive_letter%:\Ajay_prefix\wget_files\d3d\vkd3d-proton-%version%.tar.zst" -o"C:\windows\temp"
 
 :: rem Extract .tar
 "C:\Program Files\7z24.09-zstd-x64\7z.exe" x "C:\windows\temp\vkd3d-proton-%version%.tar" -o"C:\windows\temp"
@@ -65,7 +67,7 @@ goto end
 echo.
 setlocal EnableDelayedExpansion
 set count=0
-for /f "tokens=2 delims=:" %%A in ('findstr /i "tag_name" D:/Ajay_prefix/wget_files/temp/all_releases.json 2^>nul') do (
+for /f "tokens=2 delims=:" %%A in ('findstr /i "tag_name" %drive_letter%:/Ajay_prefix/wget_files/temp/all_releases.json 2^>nul') do (
     set "ver=%%A"
     set "ver=!ver:,=!"
     set "ver=!ver:"=!"
--- Start Menu/Ajay Start Menu/1.components/Vkd3d/vkd3d-2.14.1.bat
+++ Start Menu/Ajay Start Menu/1.components/Vkd3d/vkd3d-2.14.1.bat
@@ -1,11 +1,13 @@
 @echo off
+call "C:\Windows\Ajay_drive.bat" >nul 2>&1
+if not defined drive_letter set drive_letter=D
 color 0a
 echo *** wait....***
 echo *** deleting temp files...***
-rmdir /S /Q "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
+rmdir /S /Q "%drive_letter%:/Ajay_prefix/wget_files/temp" >NUL 2>&1
 rmdir /S /Q "C:/windows/temp" >NUL 2>&1
 mkdir "C:/windows/temp" >NUL 2>&1
-mkdir "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
+mkdir "%drive_letter%:/Ajay_prefix/wget_files/temp" >NUL 2>&1
 echo *** deleted temp files ***
 echo.
 echo *** script made by Ajay ***
@@ -14,9 +16,9 @@ echo *** script made by Ajay ***
 color 0A
 echo.
 echo *** Downloading vkd3d-2.14.1  ***
-IF NOT EXIST "D:\Ajay_prefix\wget_files\d3d\vkd3d-proton-2.14.1.tar.zst" (
-    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://github.com/HansKristian-Work/vkd3d-proton/releases/download/v2.14.1/vkd3d-proton-2.14.1.tar.zst
-    copy /s /y D:\Ajay_prefix\wget_files\temp\vkd3d-proton-2.14.1.tar.zst D:\Ajay_prefix\wget_files\d3d\vkd3d-proton-2.14.1.tar.zst /E /H /C /I
+IF NOT EXIST "%drive_letter%:\Ajay_prefix\wget_files\d3d\vkd3d-proton-2.14.1.tar.zst" (
+    wget -q --show-progress -P %drive_letter%:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://github.com/HansKristian-Work/vkd3d-proton/releases/download/v2.14.1/vkd3d-proton-2.14.1.tar.zst
+    copy /s /y %drive_letter%:\Ajay_prefix\wget_files\temp\vkd3d-proton-2.14.1.tar.zst %drive_letter%:\Ajay_prefix\wget_files\d3d\vkd3d-proton-2.14.1.tar.zst /E /H /C /I
 ) ELSE (
     ECHO vkd3d-2.14.1 file already exists.
 )
@@ -26,7 +28,7 @@ IF NOT EXIST "D:\Ajay_prefix\wget_files\d3d\vkd3d-proton-2.14.1.tar.zst" (
 color 1f
 echo *** Extracting ...***
 
-D:\Ajay_prefix\.Resources\winrar.exe x D:\Ajay_prefix\wget_files\d3d\vkd3d-proton-2.14.1.tar.zst C:\windows\temp\ -r -y >NUL 2>&1
+%drive_letter%:\Ajay_prefix\.Resources\winrar.exe x %drive_letter%:\Ajay_prefix\wget_files\d3d\vkd3d-proton-2.14.1.tar.zst C:\windows\temp\ -r -y >NUL 2>&1
 echo *** installing...***
 ren C:\windows\temp\vkd3d-proton-2.14.1\x64 system32
 ren C:\windows\temp\vkd3d-proton-2.14.1\x86 syswow64
