d9960246b8694d4628d9d083c5319981ff85e001
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-05 11:23:11 +0530

    Add files via upload

--- Start Menu/Ajay Start Menu/1.components/Wrapper (install it after any dxvk)/dgVoodoo/install dgvoodoo2-v2.64 (fix ddraw dx8 and nglide).bat
+++ Start Menu/Ajay Start Menu/1.components/Wrapper (install it after any dxvk)/dgVoodoo/install dgvoodoo2-v2.64 (fix ddraw dx8 and nglide).bat
@@ -1,11 +1,13 @@
 @echo off
+call "C:\Windows\Ajay_drive.bat" >nul 2>&1
+if not defined drive_letter set drive_letter=D
 set "installname=dgvoodoo2-v2.64"
 :: Remove the prefix "dxvk-" and keep the part after it
 color 0a
 echo *** wait....***
 echo *** deleting temp files...***
-rmdir /S /Q "D:/Ajay_prefix/wget_files/temp"
-mkdir "D:/Ajay_prefix/wget_files/temp"
+rmdir /S /Q "%drive_letter%:/Ajay_prefix/wget_files/temp"
+mkdir "%drive_letter%:/Ajay_prefix/wget_files/temp"
 del "C:\windows\system32\dgVoodooCpl.exe" >NUL 2>&1
 del "C:\windows\syswow64\dgVoodooCpl.exe" >NUL 2>&1
 del "C:\windows\syswow64\dgVoodoo.conf" >NUL 2>&1
@@ -18,9 +20,9 @@ echo *** script made by Ajay ***
 color 0A
 echo.
 echo *** Downloading %installname%  ***
-IF NOT EXIST "D:\Ajay_prefix\wget_files\d3d\%installname%.7z" (
-    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/%installname%.7z
-    copy /s /y D:\Ajay_prefix\wget_files\temp\%installname%.7z D:\Ajay_prefix\wget_files\d3d\%installname%.7z /E /H /C /I
+IF NOT EXIST "%drive_letter%:\Ajay_prefix\wget_files\d3d\%installname%.7z" (
+    wget -q --show-progress -P %drive_letter%:/Ajay_prefix/wget_files/temp/ https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/%installname%.7z
+    copy /s /y %drive_letter%:\Ajay_prefix\wget_files\temp\%installname%.7z %drive_letter%:\Ajay_prefix\wget_files\d3d\%installname%.7z /E /H /C /I
 ) ELSE (
     ECHO %installname% file already exists.
 )
@@ -28,9 +30,9 @@ IF NOT EXIST "D:\Ajay_prefix\wget_files\d3d\%installname%.7z" (
 :: Simulate download progress only for subsequent parts
 color 1f
 echo *** Extracting ...***
-D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\d3d\%installname%.7z -oD:\Ajay_prefix\wget_files\temp\ -p-q -r -y >NUL 2>&1
+%drive_letter%:\Ajay_prefix\.Resources\7z.exe x %drive_letter%:\Ajay_prefix\wget_files\d3d\%installname%.7z -o%drive_letter%:\Ajay_prefix\wget_files\temp\ -p-q -r -y >NUL 2>&1
 echo *** installing...***
-Xcopy /s /y D:\Ajay_prefix\wget_files\temp\syswow64\ C:\windows\syswow64\ /E /H /C /I
+Xcopy /s /y %drive_letter%:\Ajay_prefix\wget_files\temp\syswow64\ C:\windows\syswow64\ /E /H /C /I
 echo ************************************************
 echo Note - nglide dlls are replaced by dgvoodoo. you need to install nglide again to use it. you should delete all nglide related dlls from the game folder for dgvoodoo effect.
 
--- Start Menu/Ajay Start Menu/1.components/Wrapper (install it after any dxvk)/dgVoodoo/open dgvoodoo2_81_3_cpl.exe(conf).bat
+++ Start Menu/Ajay Start Menu/1.components/Wrapper (install it after any dxvk)/dgVoodoo/open dgvoodoo2_81_3_cpl.exe(conf).bat
@@ -1,11 +1,13 @@
 @echo off
+call "C:\Windows\Ajay_drive.bat" >nul 2>&1
+if not defined drive_letter set drive_letter=D
 set "installname=dgVoodoo2_81_3_cpl"
 :: Remove the prefix "dxvk-" and keep the part after it
 color 0a
 echo *** wait....***
 echo *** deleting temp files and old dgvoodoo conf ...***
-rmdir /S /Q "D:/Ajay_prefix/wget_files/temp"
-mkdir "D:/Ajay_prefix/wget_files/temp"
+rmdir /S /Q "%drive_letter%:/Ajay_prefix/wget_files/temp"
+mkdir "%drive_letter%:/Ajay_prefix/wget_files/temp"
 del "C:\windows\system32\dgVoodooCpl.exe" >NUL 2>&1
 del "C:\windows\syswow64\dgVoodooCpl.exe" >NUL 2>&1
 del "C:\windows\syswow64\dgVoodoo.conf" >NUL 2>&1
@@ -19,9 +21,9 @@ echo *** script made by Ajay ***
 color 0A
 echo.
 echo *** Downloading %installname%  ***
-IF NOT EXIST "D:\Ajay_prefix\wget_files\d3d\%installname%.7z" (
-    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/%installname%.7z
-    copy /s /y D:\Ajay_prefix\wget_files\temp\%installname%.7z D:\Ajay_prefix\wget_files\d3d\%installname%.7z /E /H /C /I
+IF NOT EXIST "%drive_letter%:\Ajay_prefix\wget_files\d3d\%installname%.7z" (
+    wget -q --show-progress -P %drive_letter%:/Ajay_prefix/wget_files/temp/ https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/%installname%.7z
+    copy /s /y %drive_letter%:\Ajay_prefix\wget_files\temp\%installname%.7z %drive_letter%:\Ajay_prefix\wget_files\d3d\%installname%.7z /E /H /C /I
 ) ELSE (
     ECHO %installname% file already exists.
 )
@@ -29,9 +31,9 @@ IF NOT EXIST "D:\Ajay_prefix\wget_files\d3d\%installname%.7z" (
 :: Simulate download progress only for subsequent parts
 color 1f
 echo *** Extracting ...***
-D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\d3d\%installname%.7z -oD:\Ajay_prefix\wget_files\temp\ -p-q -r -y >NUL 2>&1
+%drive_letter%:\Ajay_prefix\.Resources\7z.exe x %drive_letter%:\Ajay_prefix\wget_files\d3d\%installname%.7z -o%drive_letter%:\Ajay_prefix\wget_files\temp\ -p-q -r -y >NUL 2>&1
 echo *** installing...***
-Xcopy /s /y D:\Ajay_prefix\wget_files\temp\system32\ C:\windows\system32\ /E /H /C /I
+Xcopy /s /y %drive_letter%:\Ajay_prefix\wget_files\temp\system32\ C:\windows\system32\ /E /H /C /I
 
 echo opening dgVoodoocpl.exe ...
 timeout.exe 2 /nobreak >NUL 2>&1
