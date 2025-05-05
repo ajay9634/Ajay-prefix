000937366b866ed18a96f6d260db2fd3bd0c45b1
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-05 11:04:42 +0530

    Add files via upload

--- Start Menu/Ajay Start Menu/1.components/Misc/Export applied env variables info as txt.bat
+++ Start Menu/Ajay Start Menu/1.components/Misc/Export applied env variables info as txt.bat
@@ -1,7 +1,9 @@
 @echo off
-echo Exporting all applied env variables info to drive D as txt ...
+call "C:\Windows\Ajay_drive.bat" >nul 2>&1
+if not defined drive_letter set drive_letter=D
+echo Exporting all applied env variables info to drive %drive_letter% as txt ...
 set /p fname=Type name : 
-set outfile=D:\%fname% environment variables.txt
+set outfile=%drive_letter%:\%fname% environment variables.txt
 echo Saving environment variables to %outfile%...
 set > "%outfile%"
 echo Done!
