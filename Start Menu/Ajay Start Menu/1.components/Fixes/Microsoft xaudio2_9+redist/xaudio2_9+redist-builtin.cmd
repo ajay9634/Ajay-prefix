622cde21f13382aed027a936a763bc245f36797a
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-05 10:49:35 +0530

    Add files via upload

---
+++ Start Menu/Ajay Start Menu/1.components/Fixes/Microsoft xaudio2_9+redist/xaudio2_9+redist-builtin.cmd
@@ -0,0 +1,8 @@
+@echo off
+echo updating registry
+reg add "HKCU\Software\Wine\DllOverrides" /v "xaudio2_9" /t REG_SZ /d "builtin,native" /f >nul 2>&1
+reg add "HKCU\Software\Wine\DllOverrides" /v "xaudio2_9redist" /t REG_SZ /d "builtin,native" /f >nul 2>&1
+timeout.exe /t 6 /nobreak >nul 2>&1
+reg add "HKCU\Software\Wine\DllOverrides" /v "xaudio2_9" /t REG_SZ /d "builtin,native" /f >nul 2>&1
+reg add "HKCU\Software\Wine\DllOverrides" /v "xaudio2_9redist" /t REG_SZ /d "builtin,native" /f >nul 2>&1
+timeout.exe /t 4 /nobreak >nul 2>&1
