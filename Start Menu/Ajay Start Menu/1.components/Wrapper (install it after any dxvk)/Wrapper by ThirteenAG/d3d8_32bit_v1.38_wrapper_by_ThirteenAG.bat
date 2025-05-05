a331c616accae6005b084bbb2aa05064eac7e2dc
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-05 11:21:46 +0530

    Add files via upload

--- Start Menu/Ajay Start Menu/1.components/Wrapper (install it after any dxvk)/Wrapper by ThirteenAG/d3d8_32bit_v1.38_wrapper_by_ThirteenAG.bat
+++ Start Menu/Ajay Start Menu/1.components/Wrapper (install it after any dxvk)/Wrapper by ThirteenAG/d3d8_32bit_v1.38_wrapper_by_ThirteenAG.bat
@@ -1,21 +1,23 @@
 @echo off
+call "C:\Windows\Ajay_drive.bat" >nul 2>&1
+if not defined drive_letter set drive_letter=D
 color 0a
 echo *** d3d wrapper by ThirteenAG ***
 echo *** wait....***
 echo *** deleting temp files...***
-rmdir /S /Q "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
+rmdir /S /Q "%drive_letter%:/Ajay_prefix/wget_files/temp" >NUL 2>&1
 rmdir /S /Q "C:/windows/temp" >NUL 2>&1
 mkdir "C:/windows/temp" >NUL 2>&1
-mkdir "D:/Ajay_prefix/wget_files/temp" >NUL 2>&1
+mkdir "%drive_letter%:/Ajay_prefix/wget_files/temp" >NUL 2>&1
 echo *** deleted temp files ***
 echo *** Script made by Ajay ***
 
 :: Downloading d3d8_32bit_v1.38_wrapper_by_ThirteenAG if it doesn't exist
 echo *** Checking for d3d8_32bit_v1.38_wrapper_by_ThirteenAG ***
-IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\d3d8_32bit_v1.38_wrapper_by_ThirteenAG.7z" (
+IF NOT EXIST "%drive_letter%:\Ajay_prefix\wget_files\Files\d3d8_32bit_v1.38_wrapper_by_ThirteenAG.7z" (
     echo *** Downloading d3d8_32bit_v1.38_wrapper_by_ThirteenAG...***
-    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/d3d8_32bit_v1.38_wrapper_by_ThirteenAG.7z
-    copy /y D:\Ajay_prefix\wget_files\temp\d3d8_32bit_v1.38_wrapper_by_ThirteenAG.7z D:\Ajay_prefix\wget_files\Files\
+    wget -q --show-progress -P %drive_letter%:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/d3d8_32bit_v1.38_wrapper_by_ThirteenAG.7z
+    copy /y %drive_letter%:\Ajay_prefix\wget_files\temp\d3d8_32bit_v1.38_wrapper_by_ThirteenAG.7z %drive_letter%:\Ajay_prefix\wget_files\Files\
 ) ELSE (
     echo file already exists.
 )
@@ -23,7 +25,7 @@ IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\d3d8_32bit_v1.38_wrapper_by_Thirte
 :: Extracting NSIS
 color 1f
 echo *** Extracting d3d8_32bit_v1.38_wrapper_by_ThirteenAG ***
-D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\Files\d3d8_32bit_v1.38_wrapper_by_ThirteenAG.7z -oD:\Ajay_prefix\wget_files\temp\ -p-q -r -y >NUL 2>&1
+%drive_letter%:\Ajay_prefix\.Resources\7z.exe x %drive_letter%:\Ajay_prefix\wget_files\Files\d3d8_32bit_v1.38_wrapper_by_ThirteenAG.7z -o%drive_letter%:\Ajay_prefix\wget_files\temp\ -p-q -r -y >NUL 2>&1
 
 IF %ERRORLEVEL% NEQ 0 (
     echo Extraction failed. Please check the file and try again.
@@ -37,7 +39,7 @@ echo msgbox "Hello! Extract it in game exe folder then you can edit ini !" , vbi
 start /b cscript //nologo %tmp%\tmp.vbs
 timeout.exe /t 1 >nul
 del %tmp%\tmp.vbs
-Start D:\Ajay_prefix\wget_files\temp\d3d8_32bit_v1.38_wrapper_by_ThirteenAG.exe
+Start %drive_letter%:\Ajay_prefix\wget_files\temp\d3d8_32bit_v1.38_wrapper_by_ThirteenAG.exe
 timeout.exe /t 3 >nul
 
 echo ************************************************
