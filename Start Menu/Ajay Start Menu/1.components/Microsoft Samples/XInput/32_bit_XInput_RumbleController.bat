75fda7b6b07a3ad46dc48aedcbf0914e9a9d7c7d
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-05 10:59:12 +0530

    Add files via upload

--- Start Menu/Ajay Start Menu/1.components/Microsoft Samples/XInput/32_bit_XInput_RumbleController.bat
+++ Start Menu/Ajay Start Menu/1.components/Microsoft Samples/XInput/32_bit_XInput_RumbleController.bat
@@ -1,18 +1,20 @@
 @echo off
+call "C:\Windows\Ajay_drive.bat" >nul 2>&1
+if not defined drive_letter set drive_letter=D
 color 0a
-if exist "D:\Ajay_prefix\wget_files\temp2\Microsoft_Samples\C++\XInput\Bin\x86\RumbleController.exe" goto Run
+if exist "%drive_letter%:\Ajay_prefix\wget_files\temp2\Microsoft_Samples\C++\XInput\Bin\x86\RumbleController.exe" goto Run
 
 echo *** deleting temp2 files...***
-rmdir /S /Q "D:/Ajay_prefix/wget_files/temp2" >NUL 2>&1
-mkdir "D:/Ajay_prefix/wget_files/temp2" >NUL 2>&1
+rmdir /S /Q "%drive_letter%:/Ajay_prefix/wget_files/temp2" >NUL 2>&1
+mkdir "%drive_letter%:/Ajay_prefix/wget_files/temp2" >NUL 2>&1
 echo *** Script made by Ajay ***
 
 :: Downloading Microsoft Samples if it doesn't exist
 echo *** Checking for Microsoft Samples ***
-IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\Microsoft_Samples.7z" (
+IF NOT EXIST "%drive_letter%:\Ajay_prefix\wget_files\Files\Microsoft_Samples.7z" (
     echo *** Downloading Microsoft Samples...***
-    wget -q --show-progress -P D:/Ajay_prefix/wget_files/temp2/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Microsoft_Samples.7z
-    copy /y D:\Ajay_prefix\wget_files\temp2\Microsoft_Samples.7z D:\Ajay_prefix\wget_files\Files\
+    wget -q --show-progress -P %drive_letter%:/Ajay_prefix/wget_files/temp2/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Microsoft_Samples.7z
+    copy /y %drive_letter%:\Ajay_prefix\wget_files\temp2\Microsoft_Samples.7z %drive_letter%:\Ajay_prefix\wget_files\Files\
 ) ELSE (
     echo *** File already exists.***
 )
@@ -20,7 +22,7 @@ IF NOT EXIST "D:\Ajay_prefix\wget_files\Files\Microsoft_Samples.7z" (
 :: Extracting Microsoft Samples
 color 1f
 echo *** Extracting Microsoft Samples ***
-D:\Ajay_prefix\.Resources\7z.exe x D:\Ajay_prefix\wget_files\Files\Microsoft_Samples.7z -oD:\Ajay_prefix\wget_files\temp2\ -p-q -r -y >NUL 2>&1
+%drive_letter%:\Ajay_prefix\.Resources\7z.exe x %drive_letter%:\Ajay_prefix\wget_files\Files\Microsoft_Samples.7z -o%drive_letter%:\Ajay_prefix\wget_files\temp2\ -p-q -r -y >NUL 2>&1
 
 IF %ERRORLEVEL% NEQ 0 (
     echo *** Extraction failed. Please check the file and try again.***
@@ -31,7 +33,7 @@ IF %ERRORLEVEL% NEQ 0 (
 :: Launching the Installer
 :Run
 echo *** Opening Microsoft Samples ***
-Start /wait "" D:\Ajay_prefix\wget_files\temp2\Microsoft_Samples\C++\XInput\Bin\x86\RumbleController.exe
+Start /wait "" %drive_letter%:\Ajay_prefix\wget_files\temp2\Microsoft_Samples\C++\XInput\Bin\x86\RumbleController.exe
 
 echo ************************************************
 timeout.exe /t 3 >nul
