0ae77898768b22a4baa8b9ada81d7e84225a4809
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-05 10:53:01 +0530

    Add files via upload

--- Start Menu/Ajay Start Menu/1.components/Games-fixes/Patches/arx fatalis.bat
+++ Start Menu/Ajay Start Menu/1.components/Games-fixes/Patches/arx fatalis.bat
@@ -1,4 +1,6 @@
 @echo off
+call "C:\Windows\Ajay_drive.bat" >nul 2>&1
+if not defined drive_letter set drive_letter=D
 color 1f
 echo ==================================
 echo ***** arx-libertatis-1.2.1 (size-10mb) *****
@@ -29,26 +31,26 @@ ECHO *** Installing arx-libertatis-1.2.1 4.0...***
 echo.
 echo *** Script made by Ajay ***
 echo.
-rmdir /S /Q "D:/Ajay_prefix/wget_files/temp"
-mkdir "D:/Ajay_prefix/wget_files/temp"
+rmdir /S /Q "%drive_letter%:/Ajay_prefix/wget_files/temp"
+mkdir "%drive_letter%:/Ajay_prefix/wget_files/temp"
 
 :: Check if the .din backup file exists
-IF EXIST "D:/Ajay_prefix/wget_files/Game-fixes/arx-libertatis-1.2.1-windows.din" (
+IF EXIST "%drive_letter%:/Ajay_prefix/wget_files/Game-fixes/arx-libertatis-1.2.1-windows.din" (
     ECHO *** backup file found, Now running the installer...***
-    copy "D:/Ajay_prefix/wget_files/Game-fixes/arx-libertatis-1.2.1-windows.din" "D:/Ajay_prefix/wget_files/temp/arx-libertatis-1.2.1-windows.exe"
+    copy "%drive_letter%:/Ajay_prefix/wget_files/Game-fixes/arx-libertatis-1.2.1-windows.din" "%drive_letter%:/Ajay_prefix/wget_files/temp/arx-libertatis-1.2.1-windows.exe"
 ) ELSE (
     echo.
     ECHO ! backup file not found, downloading...
     echo.
-    wget -q --show-progress -P "D:/Ajay_prefix/wget_files/temp/" --progress=dot:mega https://arx-libertatis.org/files/arx-libertatis-1.2.1-windows.exe
-    copy "D:/Ajay_prefix/wget_files/temp/arx-libertatis-1.2.1-windows.exe" "D:/Ajay_prefix/wget_files/Game-fixes/arx-libertatis-1.2.1-windows.din"
+    wget -q --show-progress -P "%drive_letter%:/Ajay_prefix/wget_files/temp/" --progress=dot:mega https://arx-libertatis.org/files/arx-libertatis-1.2.1-windows.exe
+    copy "%drive_letter%:/Ajay_prefix/wget_files/temp/arx-libertatis-1.2.1-windows.exe" "%drive_letter%:/Ajay_prefix/wget_files/Game-fixes/arx-libertatis-1.2.1-windows.din"
     echo.
     ECHO *** Download complete, installing...**"
     echo.
 )
 
 :: Run the installer
-Start /wait D:/Ajay_prefix/wget_files/temp/arx-libertatis-1.2.1-windows.exe
+Start /wait %drive_letter%:/Ajay_prefix/wget_files/temp/arx-libertatis-1.2.1-windows.exe
 
 ECHO *******
 exit
