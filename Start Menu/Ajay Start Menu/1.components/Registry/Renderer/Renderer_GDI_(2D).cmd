c4ab1410a5f2935efaffc24318acf020567874c4
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-08 07:46:16 +0530

    Add files via upload

--- Start Menu/Ajay Start Menu/1.components/Registry/Renderer/Renderer_GDI_(2D).cmd
+++ Start Menu/Ajay Start Menu/1.components/Registry/Renderer/Renderer_GDI_(2D).cmd
@@ -5,10 +5,11 @@ echo Setting the "renderer" to "gdi" in the registry...
 reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "renderer" /t REG_SZ /d "gdi" /f >nul 2>&1
 
 :: Wait for 10 seconds silently
-timeout.exe /t 10 /nobreak >nul 2>&1
+timeout.exe /t 6 /nobreak >nul 2>&1
 
 :: Reapply the registry setting after the delay
 reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "renderer" /t REG_SZ /d "gdi" /f >nul 2>&1
 
+timeout.exe 4 /nobreak >nul 2>&1
 echo The registry modification for "renderer" has been applied successfully.
 exit
--- Start Menu/Ajay Start Menu/1.components/Registry/Renderer/Renderer_OpenGL.cmd
+++ Start Menu/Ajay Start Menu/1.components/Registry/Renderer/Renderer_OpenGL.cmd
@@ -5,10 +5,10 @@ echo Setting the "renderer" to "gl" in the registry...
 reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "renderer" /t REG_SZ /d "gl" /f >nul 2>&1
 
 :: Wait for 10 seconds silently
-timeout.exe /t 10 /nobreak >nul 2>&1
+timeout.exe /t 6 /nobreak >nul 2>&1
 
 :: Reapply the registry setting after the delay
 reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "renderer" /t REG_SZ /d "gl" /f >nul 2>&1
-
+timeout.exe 4 /nobreak >nul 2>&1
 echo The registry modification for "renderer" has been applied successfully.
 exit
