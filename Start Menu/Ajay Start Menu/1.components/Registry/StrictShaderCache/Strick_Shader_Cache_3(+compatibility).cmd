868cc47f9ec1eb87a90c2eedc0c5e1084b0f826d
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-08 07:46:54 +0530

    Add files via upload

--- Start Menu/Ajay Start Menu/1.components/Registry/StrictShaderCache/Strick_Shader_Cache_3(+compatibility).cmd
+++ Start Menu/Ajay Start Menu/1.components/Registry/StrictShaderCache/Strick_Shader_Cache_3(+compatibility).cmd
@@ -5,10 +5,10 @@ echo Setting "strict_shader_math" to "dword:00000003" in the registry...
 reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "strict_shader_math" /t REG_DWORD /d 3 /f >nul 2>&1
 
 :: Wait for 10 seconds silently
-timeout.exe /t 10 /nobreak >nul 2>&1
+timeout.exe /t 6 /nobreak >nul 2>&1
 
 :: Reapply the registry setting after the delay
 reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "strict_shader_math" /t REG_DWORD /d 3 /f >nul 2>&1
-
+timeout.exe /t 4 /nobreak >nul 2>&1
 echo The registry modification for "strict_shader_math" has been applied successfully.
 exit
