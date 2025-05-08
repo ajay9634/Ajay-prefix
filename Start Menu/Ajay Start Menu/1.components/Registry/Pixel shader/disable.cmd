bc4f444b94d33373a88c825702f9234ec8d84232
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-08 07:45:45 +0530

    Add files via upload

--- Start Menu/Ajay Start Menu/1.components/Registry/Pixel shader/disable.cmd
+++ Start Menu/Ajay Start Menu/1.components/Registry/Pixel shader/disable.cmd
@@ -1,7 +1,8 @@
 @echo off
 echo Disabling Pixel Shader in Wine registry...
 reg add "HKCU\Software\Wine\Direct3D" /v PixelShaderMode /t REG_SZ /d disabled /f >nul 2>&1
-timeout.exe 10 /nobreak >nul 2>&1
+timeout.exe 6 /nobreak >nul 2>&1
 reg add "HKCU\Software\Wine\Direct3D" /v PixelShaderMode /t REG_SZ /d disabled /f >nul 2>&1
+timeout.exe 4 /nobreak >nul 2>&1
 echo Pixel Shader has been disabled.
 exit /b
--- Start Menu/Ajay Start Menu/1.components/Registry/Pixel shader/enable.cmd
+++ Start Menu/Ajay Start Menu/1.components/Registry/Pixel shader/enable.cmd
@@ -1,7 +1,8 @@
 @echo off
 echo Enabling Pixel Shader in Wine registry...
 reg add "HKCU\Software\Wine\Direct3D" /v PixelShaderMode /t REG_SZ /d enabled /f >nul 2>&1
-timeout.exe 10 /nobreak >nul 2>&1
+timeout.exe 6 /nobreak >nul 2>&1
 reg add "HKCU\Software\Wine\Direct3D" /v PixelShaderMode /t REG_SZ /d enabled /f >nul 2>&1
+timeout.exe 4 /nobreak >nul 2>&1
 echo Pixel Shader has been enabled.
 exit /b
