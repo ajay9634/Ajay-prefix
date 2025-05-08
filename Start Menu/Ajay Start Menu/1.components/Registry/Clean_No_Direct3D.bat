fc214bb862f1850c6337069488506c9863a91713
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-08 07:56:23 +0530

    Add files via upload

--- Start Menu/Ajay Start Menu/1.components/Registry/Clean_No_Direct3D.bat
+++ Start Menu/Ajay Start Menu/1.components/Registry/Clean_No_Direct3D.bat
@@ -2,3 +2,6 @@
 color 84
 echo Cleaning...
 reg delete "HKEY_CURRENT_USER\Software\Wine\Direct3D" /va /f >NUL 2>&1
+timeout.exe /t 6 /nobreak >nul 2>&1
+reg delete "HKEY_CURRENT_USER\Software\Wine\Direct3D" /va /f >NUL 2>&1
+timeout.exe /t 4 /nobreak >nul 2>&1
--- Start Menu/Ajay Start Menu/1.components/Registry/Show all installed keys Direct3D.bat
+++ Start Menu/Ajay Start Menu/1.components/Registry/Show all installed keys Direct3D.bat
@@ -3,3 +3,5 @@ color 0b
 echo Open...
 reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Applets\Regedit /v LastKey /t REG_SZ /d "HKEY_CURRENT_USER\Software\Wine\Direct3D" /f >NUL 2>&1
 start C:\windows\regedit.exe
+timeout.exe /t 3 /nobreak >nul 2>&1
+
