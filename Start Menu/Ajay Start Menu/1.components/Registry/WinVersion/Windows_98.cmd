15532dae5a9b3f372ec2dd3c3790bc193740c04b
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-08 07:40:16 +0530

    Delete Start Menu/Ajay Start Menu/1.components/Registry/WinVersion directory

--- Start Menu/Ajay Start Menu/1.components/Registry/WinVersion/Windows_98.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Setting Wine version to win98...
-reg add "HKEY_CURRENT_USER\Software\Wine" /v "Version" /t REG_SZ /d "win98" /f >nul 2>&1
-
-timeout.exe /t 10 /nobreak >nul 2>&1
-
-reg add "HKEY_CURRENT_USER\Software\Wine" /v "Version" /t REG_SZ /d "win98" /f >nul 2>&1
-echo Wine version has been set to win98 successfully.
-exit
