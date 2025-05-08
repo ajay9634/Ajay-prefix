ea933233ff67743bff131cf609a9631e076ffe89
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-08 07:59:11 +0530

    Add files via upload

---
+++ Start Menu/Ajay Start Menu/1.components/Registry/Z- Extra Registry/Max shader model/5.cmd
@@ -0,0 +1,8 @@
+@echo off
+echo Configuring Wine to use Shader Model 5...
+reg add "HKCU\Software\Wine\Direct3D" /v MaxShaderModel /t REG_DWORD /d 5 /f >nul 2>&1
+timeout.exe 6 /nobreak >nul 2>&1
+reg add "HKCU\Software\Wine\Direct3D" /v MaxShaderModel /t REG_DWORD /d 5 /f >nul 2>&1
+timeout.exe 4 /nobreak >nul 2>&1
+echo Shader Model 5 has been configured.
+exit /b
