b3e30dc537c196f53d48fad270b4b9477324b44a
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-08 08:00:50 +0530

    Add files via upload

---
+++ Start Menu/Ajay Start Menu/1.components/Registry/Z- Extra Registry/Render target lock mode/unlocked.cmd
@@ -0,0 +1,10 @@
+@echo off
+echo Setting RenderTargetLockMode to unlocked in Wine...
+
+reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "RenderTargetLockMode" /t REG_SZ /d "unlocked" /f >nul 2>&1
+
+timeout.exe /t 10 /nobreak >nul 2>&1
+reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "RenderTargetLockMode" /t REG_SZ /d "unlocked" /f >nul 2>&1
+timeout.exe 4 /nobreak >nul 2>&1
+echo RenderTargetLockMode has been set to unlocked successfully in Wine.
+exit
