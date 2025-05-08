6de2b35170c0741bfeddeb59998ee18aaade5183
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-08 07:47:24 +0530

    Add files via upload

--- Start Menu/Ajay Start Menu/1.components/Registry/UseGLSL/UseGLSL_disable (+performance).cmd
+++ Start Menu/Ajay Start Menu/1.components/Registry/UseGLSL/UseGLSL_disable (+performance).cmd
@@ -2,8 +2,9 @@
 echo Disabling GLSL in Wine's Direct3D settings...
 reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "UseGLSL" /t REG_SZ /d "disabled" /f >nul 2>&1
 
-timeout.exe /t 10 /nobreak >nul 2>&1
+timeout.exe /t 6 /nobreak >nul 2>&1
 
 reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "UseGLSL" /t REG_SZ /d "disabled" /f >nul 2>&1
+timeout.exe /t 4 /nobreak >nul 2>&1
 echo GLSL setting has been disabled successfully.
 exit
