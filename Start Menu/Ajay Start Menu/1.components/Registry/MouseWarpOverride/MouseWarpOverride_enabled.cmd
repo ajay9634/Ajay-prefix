460564c54d966ecfc9c52ab755bba7eb693bb902
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-08 07:44:32 +0530

    Add files via upload

--- Start Menu/Ajay Start Menu/1.components/Registry/MouseWarpOverride/MouseWarpOverride_enabled.cmd
+++ Start Menu/Ajay Start Menu/1.components/Registry/MouseWarpOverride/MouseWarpOverride_enabled.cmd
@@ -4,11 +4,11 @@ echo Setting the "MouseWarpOverride" to "enable" in the registry...
 :: Add or update the registry key silently
 reg add "HKEY_CURRENT_USER\Software\Wine\DirectInput" /v "MouseWarpOverride" /t REG_SZ /d "enable" /f >nul 2>&1
 
-:: Wait for 10 seconds silently
+:: Wait for 6 seconds silently
 timeout.exe /t 10 /nobreak >nul 2>&1
 
 :: Reapply the registry setting after the delay
 reg add "HKEY_CURRENT_USER\Software\Wine\DirectInput" /v "MouseWarpOverride" /t REG_SZ /d "enable" /f >nul 2>&1
-
+timeout.exe 4 /nobreak >nul 2>&1
 echo The registry modification for "MouseWarpOverride" has been applied successfully.
 exit
--- Start Menu/Ajay Start Menu/1.components/Registry/MouseWarpOverride/MouseWarpOverride_force.cmd
+++ Start Menu/Ajay Start Menu/1.components/Registry/MouseWarpOverride/MouseWarpOverride_force.cmd
@@ -5,10 +5,10 @@ echo Setting the "MouseWarpOverride" to "force" in the registry...
 reg add "HKEY_CURRENT_USER\Software\Wine\DirectInput" /v "MouseWarpOverride" /t REG_SZ /d "force" /f >nul 2>&1
 
 :: Wait for 10 seconds silently
-timeout.exe /t 10 /nobreak >nul 2>&1
+timeout.exe /t 6 /nobreak >nul 2>&1
 
 :: Reapply the registry setting after the delay
 reg add "HKEY_CURRENT_USER\Software\Wine\DirectInput" /v "MouseWarpOverride" /t REG_SZ /d "force" /f >nul 2>&1
-
+timeout.exe 4 /nobreak >nul 2>&1
 echo The registry modification for "MouseWarpOverride" has been applied successfully.
 exit
