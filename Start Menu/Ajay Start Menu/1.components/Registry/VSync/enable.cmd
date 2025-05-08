dc7fc541b70154475912722c8e87993edb200be4
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-08 07:40:03 +0530

    Delete Start Menu/Ajay Start Menu/1.components/Registry/VSync directory

--- Start Menu/Ajay Start Menu/1.components/Registry/VSync/enable.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Enabling VSync in Wine's Direct3D...
-reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "VSync" /t REG_SZ /d "enabled" /f >nul 2>&1
-
-timeout.exe /t 10 /nobreak >nul 2>&1
-
-reg add "HKEY_CURRENT_USER\Software\Wine\Direct3D" /v "VSync" /t REG_SZ /d "enabled" /f >nul 2>&1
-echo VSync setting has been enabled successfully.
-exit
