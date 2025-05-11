34470e495c2d8875a7a7984f732c485d5d525be5
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-11 06:37:06 +0530

    Update CFF Explorer.bat

--- Start Menu/Ajay Start Menu/1.components/Files/CFF Explorer.bat
+++ Start Menu/Ajay Start Menu/1.components/Files/CFF Explorer.bat
@@ -22,6 +22,7 @@ echo *** Extracting ...***
 %drive_letter%:\Ajay_prefix\.Resources\7z.exe x %drive_letter%:\Ajay_prefix\wget_files\Files\ExplorerSuite.7z -o%drive_letter%:\Ajay_prefix\wget_files\temp\ -r -y >NUL 2>&1
 
 echo *** Opening ExplorerSuite  ***
+call "C:\Windows\Ajay_drive.bat" >nul 2>&1
 Start "" %drive_letter%:\Ajay_prefix\wget_files\temp\ExplorerSuite.exe
 echo ************************************************
 timeout.exe /t 3 >nul
