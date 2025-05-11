4543a5484d05890e9438cccf83e35a27f84228cd
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-11 06:37:52 +0530

    Update DOSBox-SVN-Daum 64bit.bat

--- Start Menu/Ajay Start Menu/1.components/Files/DOSBox-SVN-Daum 64bit.bat
+++ Start Menu/Ajay Start Menu/1.components/Files/DOSBox-SVN-Daum 64bit.bat
@@ -22,6 +22,7 @@ echo *** Extracting ...***
 %drive_letter%:\Ajay_prefix\.Resources\7z.exe x %drive_letter%:\Ajay_prefix\wget_files\Files\DOSBox-SVN-Daum.7z -o%drive_letter%:\Ajay_prefix\wget_files\my_apps\ -r -y >NUL 2>&1
 
 echo *** opening dosbox-64bit ***
+call "C:\Windows\Ajay_drive.bat" >nul 2>&1
 Start "" %drive_letter%:\Ajay_prefix\wget_files\my_apps\DOSBox-SVN-Daum\dosbox_x64.exe
 echo ************************************************
 timeout.exe /t 3 >nul
