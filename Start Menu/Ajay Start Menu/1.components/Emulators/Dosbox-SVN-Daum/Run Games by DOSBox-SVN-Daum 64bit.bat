9882a0184ee43be7632011008f0921deb863378f
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-29 17:09:09 +0530

    Update Run Games by DOSBox-SVN-Daum 64bit.bat

--- Start Menu/Ajay Start Menu/1.components/Emulators/Dosbox-SVN-Daum/Run Games by DOSBox-SVN-Daum 64bit.bat
+++ Start Menu/Ajay Start Menu/1.components/Emulators/Dosbox-SVN-Daum/Run Games by DOSBox-SVN-Daum 64bit.bat
@@ -1,6 +1,8 @@
 @echo off
 call "C:\Windows\Ajay_drive.bat" >nul 2>&1
 if not defined drive_letter set drive_letter=D
-Start %drive_letter%:\Ajay_prefix\wget_files\my_apps\DOSBox-SVN-Daum\dosbox_x64.bat
+cd /d "%drive_letter%:\Ajay_prefix\wget_files\my_apps\DOSBox-SVN-Daum"
+Start dosbox_x64.bat
 echo ************************************************
 timeout.exe /t 3 >nul
+
