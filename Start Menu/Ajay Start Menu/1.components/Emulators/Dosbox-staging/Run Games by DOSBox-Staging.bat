c5410dedd60cc02ade58373666a5ee208b7b0d7c
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-05-29 17:06:10 +0530

    Update Run Games by DOSBox-Staging.bat

--- Start Menu/Ajay Start Menu/1.components/Emulators/Dosbox-staging/Run Games by DOSBox-Staging.bat
+++ Start Menu/Ajay Start Menu/1.components/Emulators/Dosbox-staging/Run Games by DOSBox-Staging.bat
@@ -2,6 +2,7 @@
 echo *** opening dosbox-staging ***
 call "C:\Windows\Ajay_drive.bat" >nul 2>&1
 if not defined drive_letter set drive_letter=D
-Start %drive_letter%:\Ajay_prefix\wget_files\my_apps\dosbox-staging-v0.82.1\dosbox.bat
+cd /d "%drive_letter%:\Ajay_prefix\wget_files\my_apps\dosbox-staging-v0.82.1"
+Start dosbox.bat
 echo ************************************************
 timeout.exe /t 3 >nul
