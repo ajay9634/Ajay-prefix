bb826542d36db3245fcaaa79399bf8491ad3c952
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-06-02 19:31:21 +0530

    Update msgbox.bat

--- msgbox.bat
+++ msgbox.bat
@@ -124,33 +124,35 @@ reg add "HKEY_CLASSES_ROOT\Drive\shell\open\command" /ve /d "C:\\windows\\wfm2.e
 reg add "HKEY_CLASSES_ROOT\Directory\shell\open\command" /ve /d "C:\\windows\\wfm2.exe \"%%1\"" /f >nul 2>&1
 
 echo [INFO] Please wait...
-rmdir /S /Q "C:\windows\temp\"
-mkdir "C:/windows/temp" > NUL 2>&1
-rmdir /S /Q "%drive_letter%:/Ajay_prefix/wget_files/temp" >NUL 2>&1
-mkdir "%drive_letter%:/Ajay_prefix/wget_files/temp" >NUL 2>&1
 call "C:\Windows\Ajay_drive.bat" >nul 2>&1
 if not defined drive_letter set drive_letter=D
+rmdir /S /Q "%drive_letter%:/Ajay_prefix/wget_files/temp" >NUL 2>&1
+rmdir /S /Q "C:/windows/temp" >NUL 2>&1
+mkdir "C:/windows/temp" >NUL 2>&1
+mkdir "%drive_letter%:/Ajay_prefix/wget_files/temp" >NUL 2>&1
 
 set "baseDir=%drive_letter%:\Ajay_prefix\.Resources"
 
-:: Define whitelist (exact filenames only)
+:: Whitelist of exact filenames to keep
 set "keepList=7z.din 7z.exe Resources.7z Start_Menu.7z timeout.exe update.7z WinRAR.din WinRAR.exe"
 
-
-:: Step 1: Delete unwanted files
+:: Step 1: Delete all non-whitelisted files
 for /r "%baseDir%" %%F in (*) do (
     set "delete=1"
     set "fname=%%~nxF"
-
     for %%K in (%keepList%) do (
-        if /i "%%~nxF"=="%%K" set "delete=0"
+        if /i "!fname!"=="%%K" set "delete=0"
     )
+    if !delete! equ 1 del "%%F" >nul 2>&1
+)
 
-    if !delete! equ 1 (
-        echo Deleting file: %%F
-        del "%%F" >nul 2>&1
+:: Step 2: Delete all folders (bottom-up, multiple passes)
+for /l %%I in (1,1,5) do (
+    for /f "delims=" %%D in ('dir "%baseDir%" /ad /s /b') do (
+        if /i not "%%D"=="%baseDir%" rd "%%D" >nul 2>&1
     )
 )
+
 
 :: Step 2: Remove all folders except baseDir
 :: Run multiple passes to ensure all nested folders are removed
