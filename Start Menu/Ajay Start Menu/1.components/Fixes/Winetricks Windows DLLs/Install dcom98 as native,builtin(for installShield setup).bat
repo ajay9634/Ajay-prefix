fb3d727824d4b4d72531effe594f2c9f27599d0c
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-06-04 22:40:56 +0530

    Update Install dcom98 as native,builtin(for installShield setup).bat

--- Start Menu/Ajay Start Menu/1.components/Fixes/Winetricks Windows DLLs/Install dcom98 as native,builtin(for installShield setup).bat
+++ Start Menu/Ajay Start Menu/1.components/Fixes/Winetricks Windows DLLs/Install dcom98 as native,builtin(for installShield setup).bat
@@ -64,6 +64,7 @@ timeout.exe /t 6 /nobreak >nul 2>&1
 reg add "HKEY_CURRENT_USER\Software\Wine" /v "Version" /t REG_SZ /d "win7" /f >nul 2>&1
 timeout.exe /t 4 /nobreak >nul 2>&1
 echo [INFO] Wine version has been set to win7 successfully
+echo [Note] services.exe and rpcss.exe should be running before installing installshild setup
 echo done !
 echo ************************************************
 timeout.exe 3 /nobreak
