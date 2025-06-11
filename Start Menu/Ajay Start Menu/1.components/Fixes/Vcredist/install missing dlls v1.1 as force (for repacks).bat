4e7fdd3aa8b0558fc900ed9d24e6540ab459ca9e
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-06-11 15:06:15 +0530

    Update install missing dlls v1.1 as force (for repacks).bat

--- Start Menu/Ajay Start Menu/1.components/Fixes/Vcredist/install missing dlls v1.1 as force (for repacks).bat
+++ Start Menu/Ajay Start Menu/1.components/Fixes/Vcredist/install missing dlls v1.1 as force (for repacks).bat
@@ -48,6 +48,15 @@ xcopy "%drive_letter%:\Ajay_prefix\wget_files\temp\system32\" "C:\Windows\System
 xcopy "%drive_letter%:\Ajay_prefix\wget_files\temp\syswow64\" "C:\Windows\SysWOW64\" /E /Y /H /C /I
 
 echo.
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall\Missing_dlls" /v "DisplayName" /t REG_SZ /d "Missing_dlls" /f >nul 2>&1
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall\Missing_dlls" /v "UninstallString" /t REG_SZ /d "rundll32.exe" /f >nul 2>&1
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall\Missing_dlls" /v "DisplayVersion" /t REG_SZ /d "1.1" /f >nul 2>&1
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall\Missing_dlls" /v "Publisher" /t REG_SZ /d "Ajay Prefix" /f >nul 2>&1
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall\Missing_dlls" /v "InstallLocation" /t REG_SZ /d "C:\\windows\\system32" /f >nul 2>&1
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall\Missing_dlls" /v "DisplayIcon" /t REG_SZ /d "C:\\windows\\system32\\shell32.dll" /f >nul 2>&1
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall\Missing_dlls" /v "NoModify" /t REG_DWORD /d 1 /f >nul 2>&1
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall\Missing_dlls" /v "NoRepair" /t REG_DWORD /d 1 /f >nul 2>&1
+
 echo ************************************************
 echo           Installation complete.
 echo ************************************************
