1ffbd11a94843f62aaebfa5521df80646a6d7a41
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-06-16 18:33:22 +0530

    Update NVIDIA_PhysX_9.23.1019_SystemSoftware (latest).bat

--- Start Menu/Ajay Start Menu/1.components/Physx/NVIDIA_PhysX_9.23.1019_SystemSoftware (latest).bat
+++ Start Menu/Ajay Start Menu/1.components/Physx/NVIDIA_PhysX_9.23.1019_SystemSoftware (latest).bat
@@ -3,6 +3,7 @@ call "C:\Windows\Ajay_drive.bat" >nul 2>&1
 if not defined drive_letter set drive_letter=D
 color 1f
 echo ==================================
+echo ***** Note: Launch Explorer++ to open physx launcher
 
 :choice
 echo.
