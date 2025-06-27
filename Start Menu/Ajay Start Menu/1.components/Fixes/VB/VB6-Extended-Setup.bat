9c790c72c26958d55d97055ad90b925b9a8afb92
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-06-27 13:45:08 +0530

    Update VB6-Extended-Setup.bat

--- Start Menu/Ajay Start Menu/1.components/Fixes/VB/VB6-Extended-Setup.bat
+++ Start Menu/Ajay Start Menu/1.components/Fixes/VB/VB6-Extended-Setup.bat
@@ -16,27 +16,34 @@ color 0A
 echo.
 echo *** Downloading part01.rar ***
 IF NOT EXIST "%drive_letter%:\Ajay_prefix\wget_files\Files\VB6-Extended-Setup.part1.rar" (
-    wget -q --show-progress -P %drive_letter%:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/VB6-Extended-Setup.part1.rar
+    wget -q --show-progress -P %drive_letter%:/Ajay_prefix/wget_files/temp/ --progress=dot:mega ^
+    https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/VB6-Extended-Setup.part1.rar
     copy /s /y %drive_letter%:\Ajay_prefix\wget_files\temp\VB6-Extended-Setup.part1.rar %drive_letter%:\Ajay_prefix\wget_files\Files\VB6-Extended-Setup.part1.rar /E /H /C /I
 ) ELSE (
     ECHO Part 01 already exists.
 )
 
-:: Simulate download progress only for subsequent parts
 color 0A
 echo.
 echo *** Downloading part02.rar ***
 IF NOT EXIST "%drive_letter%:\Ajay_prefix\wget_files\Files\VB6-Extended-Setup.part2.rar" (
-    wget -q --show-progress -P %drive_letter%:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/VB6-Extended-Setup.part2.rar
+    wget -q --show-progress -P %drive_letter%:/Ajay_prefix/wget_files/temp/ --progress=dot:mega ^
+    https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/VB6-Extended-Setup.part2.rar
     copy /s /y %drive_letter%:\Ajay_prefix\wget_files\temp\VB6-Extended-Setup.part2.rar %drive_letter%:\Ajay_prefix\wget_files\Files\VB6-Extended-Setup.part2.rar /E /H /C /I
 ) ELSE (
     ECHO Part 02 already exists.
 )
 
-echo.
 color 1f
+echo.
 echo *** Extracting... ***
-%drive_letter%:\Ajay_prefix\.Resources\winrar.exe x %drive_letter%:\Ajay_prefix\wget_files\Files\VB6-Extended-Setup.part1.rar %drive_letter%:\Ajay_prefix\wget_files\temp\
+%drive_letter%:\Ajay_prefix\.Resources\winrar.exe x %drive_letter%:\Ajay_prefix\wget_files\Files\VB6-Extended-Setup.part1.rar %drive_letter%:\Ajay_prefix\wget_files\temp\ >NUL 2>&1
+
+IF NOT EXIST "%drive_letter%:\Ajay_prefix\wget_files\temp\VB6-Extended-Setup.exe" (
+    echo [!] WinRAR failed. Trying 7z fallback...
+    C:\Windows\7z.exe x %drive_letter%:\Ajay_prefix\wget_files\Files\VB6-Extended-Setup.part1.rar -o%drive_letter%:\Ajay_prefix\wget_files\temp\ -y >NUL 2>&1
+)
+
 echo.
 echo *** Running the installer ***
 echo ignore or skip error
