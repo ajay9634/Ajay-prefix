e21644cd00ab396689688527e1ba970e70a5fb47
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-06-29 16:25:25 +0530

    Add files via upload

---
+++ Start Menu/Ajay Start Menu/1.components/Files/Ajay File Manager.bat
@@ -0,0 +1,35 @@
+@echo off
+call "C:\Windows\Ajay_drive.bat" >nul 2>&1
+if not defined drive_letter set drive_letter=D
+color 0a
+echo *** wait....***
+echo *** deleting temp files...***
+rmdir /S /Q "%drive_letter%:/Ajay_prefix/wget_files/temp"
+mkdir "%drive_letter%:/Ajay_prefix/wget_files/temp"
+echo *** deleted temp files ***
+echo.
+echo *** script made by Ajay ***
+
+:: Simulating bold with color and emphasis
+color 0A
+echo.
+taskkill /f /im Ajay_File_Manager.exe >nul 2>&1
+echo *** Downloading Ajay File Manager  ***
+IF NOT EXIST "%drive_letter%:\Ajay_prefix\wget_files\Files\Ajay_File_Manager.7z" (
+    wget -q --show-progress -P %drive_letter%:/Ajay_prefix/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/Ajay_File_Manager.7z
+    copy /s /y %drive_letter%:\Ajay_prefix\wget_files\temp\Ajay_File_Manager.7z %drive_letter%:\Ajay_prefix\wget_files\Files\Ajay_File_Manager.7z /E /H /C /I
+) ELSE (
+    ECHO Ajay_File_Manager file already exists.
+)
+
+:: Simulate download progress only for subsequent parts
+
+color 1f
+echo *** Extracting ...***
+%drive_letter%:\Ajay_prefix\.Resources\7z.exe x %drive_letter%:\Ajay_prefix\wget_files\Files\Ajay_File_Manager.7z -o%drive_letter%:\Ajay_prefix\wget_files\my_apps\ -pAjay9634 -r -y >NUL 2>&1
+echo *** opening ...***
+call "C:\Windows\Ajay_drive.bat" >nul 2>&1
+Start %drive_letter%:\Ajay_prefix\wget_files\my_apps\Ajay_File_Manager.exe
+
+echo ************************************************
+timeout.exe /t 3 >nul
