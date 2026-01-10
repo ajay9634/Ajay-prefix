190e6c1c2f1f5c3e5725adb4f8f8697f526307b9
Ajay <78233144+ajay9634@users.noreply.github.com>
2026-01-10 09:29:21 +0530

    Refactor msgbox.bat for improved messaging and paths
    Updated messages and paths in msgbox.bat for clarity and accuracy.

--- msgbox.bat
+++ msgbox.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal EnableDelayedExpansion
 call "C:\Windows\Ajay_drive.bat" >nul 2>&1
 if not defined drive_letter set drive_letter=D
 color 1E
@@ -12,23 +13,14 @@ set "expected_file=10.19.txt"
 REM Check if expected file is inside the archive (quietly)
 "%toolpath%" l "%archive%" %password% 2>nul | findstr /I /C:"%expected_file%" >nul 2>&1
 if errorlevel 1 (
-    echo.
-    echo [INFO] Looks like you're using an outdated version of the Ajay prefix.
-    echo [INFO] Please download the latest version from Ajay's GitHub.
-    echo [INFO] The latest version 10.19 was uploaded on 16 July 2025.
-    echo msgbox "You're using an outdated version of the Ajay prefix. Please download and install the latest version from Ajay's GitHub." , vbInformation+vbSystemModal > "%tmp%\tmp.vbs"
-    cscript //nologo "%tmp%\tmp.vbs"
-    del "%tmp%\tmp.vbs"
-    echo Removing Start Menu Files..
-    rmdir /S /Q "C:\ProgramData\Microsoft\Windows\Start Menu\Ajay Start Menu"
+    goto Installation  
 ) else (
-    echo [INFO] The latest version 10.19 was uploaded on 16 July 2025.
-    echo [INFO] You're using outdated version of scripts updater . Please Install the latest version from Ajay's GitHub.
-    echo Removing Start Menu Files..
-    rmdir /S /Q "C:\ProgramData\Microsoft\Windows\Start Menu\Ajay Start Menu"
-    @echo off
-setlocal EnableDelayedExpansion
+    goto Installation
+)
 
+:Installation
+echo [INFO] You're using outdated version of Ajay Prefix.
+echo.
 echo Downloading Latest version of Ajay Prefix Pro...
 
 set "TARGET_DIR=Z:\home\.Ajay.Prefix.Pro.v1.3"
@@ -65,7 +57,7 @@ if not exist "%TEMP_FILE%" (
 :: Create target folder if it doesn't exist
 if not exist "%TARGET_DIR%" mkdir "%TARGET_DIR%" >nul 2>&1
 
-echo Copying installer to target...
+echo Copying installer to Drive Z...
 copy /y "%TEMP_FILE%" "%TARGET_DIR%\" >nul 2>&1
 
 if not exist "%TARGET_FILE%" (
@@ -79,6 +71,9 @@ echo.
 echo Starting installer...
 start "" "%TARGET_FILE%"
 
+echo.
+echo Removing Old Start Menu Files ..
+    rmdir /S /Q "C:\ProgramData\Microsoft\Windows\Start Menu\Ajay Start Menu"
 echo.
 echo Done!
 timeout /t 3 >nul
